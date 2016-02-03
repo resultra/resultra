f9052fd45c5cb42fb99d0b126bef9fc1115ecbe3
Steve Roehling <sroehling@resultra.com>
2016-02-03 13:17:41 -0700

    Add initial support to drag and drop new fields from a gallery pane onto the layout canvas.
    The field isn't yet adding data to it's parent layout, which needs to be accomplished through JSON/AJAX with the server API.

--- webui/js/layout.js
+++ webui/js/layout.js
@@ -32,6 +32,99 @@ $(document).ready(function() {
 				  
 		
 	});
+	
+	var placeholderID = "placeholderFieldID"
+	var newFieldDraggablePlaceholderHTML = ''+
+		'<div class="ui-widget-content layoutContainer layoutField draggable resizable" id="'+placeholderID+'">' +
+			'<div class="field">'+
+				'<label>New Field</label>'+
+				'<input type="text" name="symbol" class="layoutInput" placeholder="Enter">'+
+			'</div>'+
+		'</div>`';
+	
+	$(".newField").draggable({
+		
+		revert: 'invalid',
+		cursor: "move",
+		helper: function() { 
+			 // Instead of dragging the icon in the gallery, drag a "placeholder image" of the
+			 // field itself. This allows the user to see the proporitions of the field relative
+			 // to the other elements already on the canvas.
+			 var placeholder = $(newFieldDraggablePlaceholderHTML); 
+			 // While in layout mode, disable entry into the fields
+			 // Interestingly, there's not CSS for this.
+			 placeholder.find('input').prop('disabled',true);
+			 return placeholder
+		 },
+		stack: "div", // important to keep the draggable above the other elements
+		
+		stop: function(event, ui) {
+				  var layoutPos = {
+					  x: ui.position.left,
+					  y: ui.position.top,
+				  };
+				  
+				  console.log("drag stop: id: " + event.target.id);
+				  console.log("drag stop: new position: " + JSON.stringify(layoutPos));
+      	  
+		      } // stop function
+	});
+	
+    $("#layoutCanvas").droppable({
+        accept: ".newField",
+        activeClass: "ui-state-highlight",
+        drop: function( event, ui ) {
+ 			var theClone = $(ui.helper).clone()
+			
+			// After the drag operation is complete, the draggable and resizable
+			// properties need to be initialized.
+			// TODO - This could be put into a common function, since these
+			// properties should be the same for objects loaded with the page
+			// and newly added objects.
+			theClone.draggable ({
+				cursor: "move",
+				containment: "parent",
+				clone: "original",				
+				stop: function(event, ui) {
+						  var layoutPos = {
+							  x: ui.position.left,
+							  y: ui.position.top,
+						  };
+				  
+						  console.log("drag stop: id: " + event.target.id);
+						  console.log("drag stop: new position: " + JSON.stringify(layoutPos));
+      	  
+				      } // stop function
+			})
+			theClone.resizable({
+				aspectRatio: false,
+				handles: 'e, w', // Only allow resizing horizontally
+				maxWidth: 600,
+				minWidth: 100,
+				grid: 20, // snap to grid during resize
+		
+				stop: function(event, ui) {
+				  
+						  var layoutDim = {
+							  width: ui.size.width,
+							  height: ui.size.height
+						  };
+     
+						  console.log("resize stop: id: " + event.target.id);
+				          console.log("resize stop: new dimensions:" + JSON.stringify(layoutDim));
+					  } // 
+			});
+			// Get the relative position of the field being dragged
+			// and set it within the canvas area
+			var canvasRelTop = ui.offset.top-$(this).offset().top
+			var canvasRelLeft = ui.offset.left-$(this).offset().left
+			theClone.css({top: canvasRelTop, left: canvasRelLeft, position:"absolute"});
+			$(this).append(theClone);
+		    console.log("drag stop: relTop=" + canvasRelTop + " relLeft=" + canvasRelLeft);
+        }
+    });
+	
+	
 
 	$(".layoutContainer").resizable({
 		aspectRatio: false,
@@ -65,10 +158,9 @@ $(document).ready(function() {
 	
 	$('.layoutPageDiv').layout({
 	    center__paneSelector: "#layoutCanvas",
-	    east__paneSelector:   "#propertiesSidebar"
+	    east__paneSelector:   "#propertiesSidebar",
+		west__paneSelector: "#gallerySidebar"
 	  });
-
-		alert("Document Ready: layout ID=" + layoutID)
 
 
 });
