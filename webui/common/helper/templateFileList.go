d4bf68630688a6d54090ee6627ac4dfd9189d915
Steve Roehling <sroehling@resultra.com>
2016-05-17 13:49:58 -0700

    Setup helper package with standard include mechanism for Javascript files.

---
+++ webui/common/helper/templateFileList.go
@@ -0,0 +1,3 @@
+package helper
+
+var TemplateFileList = []string{"static/common/helper/include.html"}
--- webui/common/templateFileList.go
+++ webui/common/templateFileList.go
@@ -3,6 +3,7 @@ package common
 import (
 	"resultra/datasheet/webui/common/breadCrumb"
 	"resultra/datasheet/webui/common/formulaEditor"
+	"resultra/datasheet/webui/common/helper"
 	"resultra/datasheet/webui/common/objectSelection"
 )
 
@@ -15,5 +16,6 @@ func init() {
 	TemplateFileList = append(TemplateFileList, objectSelection.TemplateFileList...)
 	TemplateFileList = append(TemplateFileList, formulaEditor.TemplateFileList...)
 	TemplateFileList = append(TemplateFileList, breadCrumb.TemplateFileList...)
+	TemplateFileList = append(TemplateFileList, helper.TemplateFileList...)
 
 }
