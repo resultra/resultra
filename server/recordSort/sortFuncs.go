ebad25fefd50359d96a1ad65f8b06adaf640f34e
Steve Roehling <sroehling@resultra.com>
2016-06-20 12:29:16 -0700

    Implement underlying sort functions for RecordValueResults

---
+++ server/recordSort/sortFuncs.go
@@ -0,0 +1,304 @@
+package recordSort
+
+import (
+	"resultra/datasheet/server/recordValue"
+	"sort"
+	"time"
+)
+
+// ByRecordValue is the type of a "less" function that defines the ordering
+// of its RecordValueResults arguments.
+type ByRecordValueLessFunc func(p1, p2 recordValue.RecordValueResults) bool
+
+type RecordValueSorter struct {
+	RecordValues   []recordValue.RecordValueResults
+	MultiSortFuncs []ByRecordValueLessFunc
+}
+
+func (recValSorter *RecordValueSorter) Len() int {
+	return len(recValSorter.RecordValues)
+}
+
+// Swap is part of sort.Interface.
+func (recValSorter *RecordValueSorter) Swap(i, j int) {
+	recValSorter.RecordValues[i], recValSorter.RecordValues[j] = recValSorter.RecordValues[j], recValSorter.RecordValues[i]
+}
+
+func (recValSorter *RecordValueSorter) Less(i, j int) bool {
+
+	// Use indirection to retrieve the corresponding record values for the
+	// given indices.
+	p, q := recValSorter.RecordValues[i], recValSorter.RecordValues[j]
+
+	// Iterate through the multi-sort functions in order to compare the
+	// 2 record values until one is found to be less than the other using
+	// the given sort criteria.
+	for _, lessFunc := range recValSorter.MultiSortFuncs {
+		switch {
+		case lessFunc(p, q):
+			// p < q => we have a decision
+			return true
+		case lessFunc(q, p):
+			// q < p => we have a decision
+			return false
+		}
+	}
+	// All comparisons to this point haven't yielded a decision; i.e., the 2 elements are equal
+	// using all the sort functions. So, return whatever the last function returns.
+	if len(recValSorter.MultiSortFuncs) > 0 {
+		return recValSorter.MultiSortFuncs[len(recValSorter.MultiSortFuncs)-1](p, q)
+	}
+	return false
+}
+
+// OrderedBy returns a Sorter that sorts using the less functions, in order.
+// Call its Sort method to sort the data.
+func OrderedBy(lessFuncs ...ByRecordValueLessFunc) *RecordValueSorter {
+	return &RecordValueSorter{
+		MultiSortFuncs: lessFuncs,
+	}
+}
+
+// Sort sorts the argument slice according to the less functions passed to OrderedBy.
+func (recValSorter *RecordValueSorter) Sort(recordValues []recordValue.RecordValueResults) {
+	recValSorter.RecordValues = recordValues
+	sort.Sort(recValSorter)
+}
+
+// sortByNumberField uses a closure to create a less sorting function conforming
+// to the ByRecordValueLessFunc type.
+func SortByNumberField(fieldID string, direction string) ByRecordValueLessFunc {
+	if direction == sortDirectionAsc {
+		lessAscFunc := func(p1, p2 recordValue.RecordValueResults) bool {
+
+			// The following assignments will try to find the value with the right
+			// type and get those values if they are the correct type. This assignment
+			// also does the right thing when no value is found or the value is the incorrect type;
+			// in both these cases the value will be returned as not found ()
+			p1NumberVal, p1Found := p1.FieldValues[fieldID].(float64)
+			p2NumberVal, p2Found := p2.FieldValues[fieldID].(float64)
+
+			if p1Found {
+				if p2Found {
+					// Both p1 & p2 are defined, so do an actual comparison of their
+					// numbers.
+					return p1NumberVal < p2NumberVal
+				} else {
+					// Value for p2 is undefined (blank) => p1 < p2 (put blanks at end)
+					return true
+				}
+			} else { // p1 undefined
+				if p2Found {
+					// p1 is undefined (blank), but p2 has a value => p2 < p1 (put blanks at end)
+					return false
+				} else {
+					// Both p1 & p2 are undefined, return whatever.
+					return false
+				}
+			}
+		}
+		return lessAscFunc
+	} else { // sortDirectionDesc
+		lessDescFunc := func(p1, p2 recordValue.RecordValueResults) bool {
+
+			p1NumberVal, p1Found := p1.FieldValues[fieldID].(float64)
+			p2NumberVal, p2Found := p2.FieldValues[fieldID].(float64)
+
+			if p1Found {
+				if p2Found {
+					// Both p1 & p2 are defined, so do an actual comparison of their
+					// numbers.
+					return p1NumberVal > p2NumberVal
+				} else {
+					return false
+				}
+			} else { // p1 undefined
+				if p2Found {
+					return true
+				} else {
+					// Both p1 & p2 are undefined, return whatever.
+					return false
+				}
+			}
+		}
+		return lessDescFunc
+	}
+}
+
+func SortByTextField(fieldID string, direction string) ByRecordValueLessFunc {
+	if direction == sortDirectionAsc {
+		lessAscFunc := func(p1, p2 recordValue.RecordValueResults) bool {
+
+			// The following assignments will try to find the value with the right
+			// type and get those values if they are the correct type. This assignment
+			// also does the right thing when no value is found or the value is the incorrect type;
+			// in both these cases the value will be returned as not found ()
+			p1Val, p1Found := p1.FieldValues[fieldID].(string)
+			p2Val, p2Found := p2.FieldValues[fieldID].(string)
+
+			if p1Found {
+				if p2Found {
+					// Both p1 & p2 are defined, so do an actual comparison of their
+					// numbers.
+					return p1Val < p2Val
+				} else {
+					// Value for p2 is undefined (blank) => p1 < p2 (blanks at end)
+					return true
+				}
+			} else { // p1 undefined
+				if p2Found {
+					// p1 is undefined (blank), but p2 has a value => p2 < p1
+					return false
+				} else {
+					// Both p1 & p2 are undefined, return whatever.
+					return false
+				}
+			}
+		}
+		return lessAscFunc
+	} else { // sortDirectionDesc
+		lessDescFunc := func(p1, p2 recordValue.RecordValueResults) bool {
+
+			p1Val, p1Found := p1.FieldValues[fieldID].(string)
+			p2Val, p2Found := p2.FieldValues[fieldID].(string)
+
+			if p1Found {
+				if p2Found {
+					// Both p1 & p2 are defined, so do an actual comparison of their
+					// numbers.
+					return p1Val > p2Val
+				} else {
+					// Value for p2 is undefined (blank) => p2 < p1
+					return false
+				}
+			} else { // p1 undefined
+				if p2Found {
+					// p1 is undefined (blank), but p2 has a value => p1 < p2
+					return true
+				} else {
+					// Both p1 & p2 are undefined, return whatever.
+					return false
+				}
+			}
+		}
+		return lessDescFunc
+	}
+}
+
+func SortByBoolField(fieldID string, direction string) ByRecordValueLessFunc {
+	if direction == sortDirectionAsc {
+		lessAscFunc := func(p1, p2 recordValue.RecordValueResults) bool {
+
+			// The following assignments will try to find the value with the right
+			// type and get those values if they are the correct type. This assignment
+			// also does the right thing when no value is found or the value is the incorrect type;
+			// in both these cases the value will be returned as not found ()
+			p1Val, p1Found := p1.FieldValues[fieldID].(bool)
+			p2Val, p2Found := p2.FieldValues[fieldID].(bool)
+
+			if p1Found {
+				if p2Found {
+					// Both p1 & p2 are defined, so do an actual comparison
+					if (p1Val == false) && (p2Val == true) {
+						return true // false < true
+					} else {
+						return false
+					}
+				} else {
+					// Value for p2 is undefined (blank) => p1 < p2 (put blanks at end)
+					return true
+				}
+			} else { // p1 undefined
+				if p2Found {
+					// p1 is undefined (blank), but p2 has a value => p1 < p2
+					return false
+				} else {
+					// Both p1 & p2 are undefined, return whatever.
+					return false
+				}
+			}
+		}
+		return lessAscFunc
+	} else { // sortDirectionDesc
+		lessDescFunc := func(p1, p2 recordValue.RecordValueResults) bool {
+
+			p1Val, p1Found := p1.FieldValues[fieldID].(bool)
+			p2Val, p2Found := p2.FieldValues[fieldID].(bool)
+
+			if p1Found {
+				if p2Found {
+					// Both p1 & p2 are defined, so do an actual comparison
+					if (p1Val == true) && (p2Val == false) {
+						return true
+					} else {
+						return false
+					}
+				} else {
+					return false
+				}
+			} else { // p1 undefined
+				if p2Found {
+					return true
+				} else {
+					return false
+				}
+			}
+		}
+		return lessDescFunc
+	}
+}
+
+func SortByTimeField(fieldID string, direction string) ByRecordValueLessFunc {
+	if direction == sortDirectionAsc {
+		lessAscFunc := func(p1, p2 recordValue.RecordValueResults) bool {
+
+			// The following assignments will try to find the value with the right
+			// type and get those values if they are the correct type. This assignment
+			// also does the right thing when no value is found or the value is the incorrect type;
+			// in both these cases the value will be returned as not found ()
+			p1Val, p1Found := p1.FieldValues[fieldID].(time.Time)
+			p2Val, p2Found := p2.FieldValues[fieldID].(time.Time)
+
+			if p1Found {
+				if p2Found {
+					// Both p1 & p2 are defined, so do an actual comparison of their
+					// numbers.
+					return p1Val.Before(p2Val)
+				} else {
+					// Value for p2 is undefined (blank) => p1 < p2 (put blanks at end)
+					return true
+				}
+			} else { // p1 undefined
+				if p2Found {
+					// p1 is undefined (blank), but p2 has a value => p2 < p1
+					return false
+				} else {
+					// Both p1 & p2 are undefined, return whatever.
+					return false
+				}
+			}
+		}
+		return lessAscFunc
+	} else { // sortDirectionDesc
+		lessDescFunc := func(p1, p2 recordValue.RecordValueResults) bool {
+
+			p1Val, p1Found := p1.FieldValues[fieldID].(time.Time)
+			p2Val, p2Found := p2.FieldValues[fieldID].(time.Time)
+
+			if p1Found {
+				if p2Found {
+					return p2Val.Before(p1Val)
+				} else {
+					return false
+				}
+			} else { // p1 undefined
+				if p2Found {
+					return true
+				} else {
+					return false // return whatever
+				}
+			}
+		}
+		return lessDescFunc
+	}
+}
---
+++ server/recordSort/sortFuncs_test.go
@@ -0,0 +1,244 @@
+package recordSort
+
+import (
+	"resultra/datasheet/server/common/testUtil"
+	"resultra/datasheet/server/record"
+	"resultra/datasheet/server/recordValue"
+	"testing"
+	"time"
+)
+
+func TestNumberValueSort(t *testing.T) {
+
+	fieldID := "fieldA"
+
+	rec1Values := record.RecFieldValues{}
+	rec1Values[fieldID] = 42.5
+	recVal1 := recordValue.RecordValueResults{
+		RecordID:    "Record_01",
+		FieldValues: rec1Values}
+
+	rec2Values := record.RecFieldValues{}
+	rec2Values[fieldID] = 100.0
+	recVal2 := recordValue.RecordValueResults{
+		RecordID:    "Record_02",
+		FieldValues: rec2Values}
+
+	// recValues initialized with records out of order
+	recValues := []recordValue.RecordValueResults{recVal2, recVal1}
+
+	fieldAsc := SortByNumberField(fieldID, sortDirectionAsc)
+	OrderedBy(fieldAsc).Sort(recValues)
+	if recValues[0].RecordID != "Record_01" {
+		t.Errorf("TestRecordValueSort: expecting Record_01 first")
+	}
+	t.Logf("TestRecordValueSort: sort asc results %+v", testUtil.EncodeJSONString(t, recValues))
+
+	fieldDesc := SortByNumberField(fieldID, sortDirectionDesc)
+	OrderedBy(fieldDesc).Sort(recValues)
+	if recValues[0].RecordID != "Record_02" {
+		t.Errorf("TestRecordValueSort: expecting Record_02 first")
+	}
+	t.Logf("TestRecordValueSort: sort desc results %+v", testUtil.EncodeJSONString(t, recValues))
+
+}
+
+func TestBlankNumberValueSort(t *testing.T) {
+
+	fieldID := "fieldA"
+
+	rec1Values := record.RecFieldValues{}
+	// field value not set.
+	recVal1 := recordValue.RecordValueResults{
+		RecordID:    "Record_01",
+		FieldValues: rec1Values}
+
+	rec2Values := record.RecFieldValues{}
+	rec2Values[fieldID] = 100.0
+	recVal2 := recordValue.RecordValueResults{
+		RecordID:    "Record_02",
+		FieldValues: rec2Values}
+
+	// recValues initialized with records out of order
+	recValues := []recordValue.RecordValueResults{recVal1, recVal2}
+
+	fieldAsc := SortByNumberField(fieldID, sortDirectionAsc)
+	OrderedBy(fieldAsc).Sort(recValues)
+	if recValues[0].RecordID != "Record_02" {
+		t.Errorf("TestRecordValueSort: expecting Record_01 first (blanks last)")
+	}
+	t.Logf("TestRecordValueSort: sort asc results %+v", testUtil.EncodeJSONString(t, recValues))
+
+	fieldDesc := SortByNumberField(fieldID, sortDirectionDesc)
+	OrderedBy(fieldDesc).Sort(recValues)
+	if recValues[0].RecordID != "Record_01" {
+		t.Errorf("TestRecordValueSort: expecting Record_02 first (blanks first)")
+	}
+	t.Logf("TestRecordValueSort: sort desc results %+v", testUtil.EncodeJSONString(t, recValues))
+
+}
+
+func TestTextValueSort(t *testing.T) {
+
+	fieldID := "fieldA"
+
+	rec1Values := record.RecFieldValues{}
+	rec1Values[fieldID] = "ABC"
+	recVal1 := recordValue.RecordValueResults{
+		RecordID:    "Record_01",
+		FieldValues: rec1Values}
+
+	rec2Values := record.RecFieldValues{}
+	rec2Values[fieldID] = "DEF"
+	recVal2 := recordValue.RecordValueResults{
+		RecordID:    "Record_02",
+		FieldValues: rec2Values}
+
+	// recValues initialized with records out of order
+	recValues := []recordValue.RecordValueResults{recVal2, recVal1}
+
+	fieldAsc := SortByTextField(fieldID, sortDirectionAsc)
+	OrderedBy(fieldAsc).Sort(recValues)
+	if recValues[0].RecordID != "Record_01" {
+		t.Errorf("TestTextValueSort: expecting Record_01 first")
+	}
+	t.Logf("TestTextValueSort: sort asc results %+v", testUtil.EncodeJSONString(t, recValues))
+
+	fieldDesc := SortByTextField(fieldID, sortDirectionDesc)
+	OrderedBy(fieldDesc).Sort(recValues)
+	if recValues[0].RecordID != "Record_02" {
+		t.Errorf("TestRecordValueSort: expecting Record_02 first")
+	}
+	t.Logf("TestTextValueSort: sort desc results %+v", testUtil.EncodeJSONString(t, recValues))
+
+}
+
+func TestBoolValueSort(t *testing.T) {
+
+	fieldID := "fieldA"
+
+	rec1Values := record.RecFieldValues{}
+	rec1Values[fieldID] = false
+	recVal1 := recordValue.RecordValueResults{
+		RecordID:    "Record_01",
+		FieldValues: rec1Values}
+
+	rec2Values := record.RecFieldValues{}
+	rec2Values[fieldID] = true
+	recVal2 := recordValue.RecordValueResults{
+		RecordID:    "Record_02",
+		FieldValues: rec2Values}
+
+	// recValues initialized with records out of order
+	recValues := []recordValue.RecordValueResults{recVal2, recVal1}
+
+	fieldAsc := SortByBoolField(fieldID, sortDirectionAsc)
+	OrderedBy(fieldAsc).Sort(recValues)
+	if recValues[0].RecordID != "Record_01" {
+		t.Errorf("TestTextValueSort: expecting Record_01 first")
+	}
+	t.Logf("TestTextValueSort: sort asc results %+v", testUtil.EncodeJSONString(t, recValues))
+
+	fieldDesc := SortByBoolField(fieldID, sortDirectionDesc)
+	OrderedBy(fieldDesc).Sort(recValues)
+	if recValues[0].RecordID != "Record_02" {
+		t.Errorf("TestRecordValueSort: expecting Record_02 first")
+	}
+	t.Logf("TestTextValueSort: sort desc results %+v", testUtil.EncodeJSONString(t, recValues))
+
+}
+
+func TestTimeValueSort(t *testing.T) {
+
+	fieldID := "fieldA"
+
+	rec1Values := record.RecFieldValues{}
+	timeFormat := "2006-Jan-02"
+	time1, _ := time.Parse(timeFormat, "2015-Dec-01")
+	rec1Values[fieldID] = time1
+	recVal1 := recordValue.RecordValueResults{
+		RecordID:    "Record_01",
+		FieldValues: rec1Values}
+
+	rec2Values := record.RecFieldValues{}
+	time2, _ := time.Parse(timeFormat, "2016-Jan-31")
+	rec2Values[fieldID] = time2
+	recVal2 := recordValue.RecordValueResults{
+		RecordID:    "Record_02",
+		FieldValues: rec2Values}
+
+	// recValues initialized with records out of order
+	recValues := []recordValue.RecordValueResults{recVal2, recVal1}
+
+	fieldAsc := SortByTimeField(fieldID, sortDirectionAsc)
+	OrderedBy(fieldAsc).Sort(recValues)
+	if recValues[0].RecordID != "Record_01" {
+		t.Errorf("TestTextValueSort: expecting Record_01 first")
+	}
+	t.Logf("TestTextValueSort: sort asc results %+v", testUtil.EncodeJSONString(t, recValues))
+
+	fieldDesc := SortByTimeField(fieldID, sortDirectionDesc)
+	OrderedBy(fieldDesc).Sort(recValues)
+	if recValues[0].RecordID != "Record_02" {
+		t.Errorf("TestRecordValueSort: expecting Record_02 first")
+	}
+	t.Logf("TestTextValueSort: sort desc results %+v", testUtil.EncodeJSONString(t, recValues))
+
+}
+
+const numberFieldID string = "numField"
+const textFieldID string = "textField"
+
+func createMultiRecordVal(recID string, textVal string, numVal float64) recordValue.RecordValueResults {
+	recValues := record.RecFieldValues{}
+	recValues[numberFieldID] = numVal
+	recValues[textFieldID] = textVal
+	recValResults := recordValue.RecordValueResults{
+		RecordID:    recID,
+		FieldValues: recValues}
+	return recValResults
+}
+
+func TestMultiFieldValueSort(t *testing.T) {
+
+	rec1Vals := createMultiRecordVal("rec1", "A", 1)
+	rec2Vals := createMultiRecordVal("rec2", "A", 2)
+	rec3Vals := createMultiRecordVal("rec3", "B", 4)
+	rec4Vals := createMultiRecordVal("rec4", "D", 3)
+
+	// recValues initialized with records out of order
+	recValues := []recordValue.RecordValueResults{rec4Vals, rec1Vals, rec3Vals, rec2Vals}
+
+	numberAsc := SortByNumberField(numberFieldID, sortDirectionAsc)
+	textAsc := SortByTextField(textFieldID, sortDirectionAsc)
+	OrderedBy(textAsc, numberAsc).Sort(recValues)
+	if recValues[0].RecordID != "rec1" {
+		t.Errorf("TestRecordValueSort: expecting rec1 first")
+	}
+	if recValues[1].RecordID != "rec2" {
+		t.Errorf("TestRecordValueSort: expecting rec2 2nd")
+	}
+	if recValues[2].RecordID != "rec3" {
+		t.Errorf("TestRecordValueSort: expecting rec3 3rd")
+	}
+	if recValues[3].RecordID != "rec4" {
+		t.Errorf("TestRecordValueSort: expecting rec4 4th")
+	}
+
+	OrderedBy(numberAsc, textAsc).Sort(recValues)
+	if recValues[0].RecordID != "rec1" {
+		t.Errorf("TestRecordValueSort: expecting rec1 first")
+	}
+	if recValues[1].RecordID != "rec2" {
+		t.Errorf("TestRecordValueSort: expecting rec2 2nd")
+	}
+	if recValues[2].RecordID != "rec4" {
+		t.Errorf("TestRecordValueSort: expecting rec4 3rd")
+	}
+	if recValues[3].RecordID != "rec3" {
+		t.Errorf("TestRecordValueSort: expecting rec3 4th")
+	}
+
+	t.Logf("TestRecordValueSort: sort asc results %+v", testUtil.EncodeJSONString(t, recValues))
+
+}
