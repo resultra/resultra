731a67aee01b0293346985b8014b28d5f439eb02
Steve Roehling <sroehling@resultra.com>
2016-07-02 06:19:15 -0700

    Migrate dialog to create new text box to Bootstrap based wizard dialog.

--- webui/form/components/textBox/newTextBoxDialog.js
+++ webui/form/components/textBox/newTextBoxDialog.js
@@ -1,104 +1,44 @@
 
 
-// Initialized below in newLayoutContainer
-var newTextBoxParams;
-
-var textBoxProgressDivID = '#newTextBoxProgress'
-
-var textBoxDialogSelector = "#newTextBox"
-
-
 function openNewTextBoxDialog(formID,parentTableID,containerParams)
 {
-	
-	// Must be the same as designForm.go - this is the common prefix on all DOM element IDs to distinguish
-	// checkbox related elements from other form elements.
-	var textBoxElemPrefix = "textBox_"
-	var dialogProgressSelector = '#' + textBoxElemPrefix + 'NewFormElemDialogProgress'
-	
-	newTextBoxParams = {
-		containerParams: containerParams,
-		containerCreated: false,
-		placeholderID: containerParams.containerID,
-		dialogBox: $( textBoxDialogSelector )
+		
+	function createNewTextBox($parentDialog, newComponentParams) {
+		jsonAPIRequest("frm/textBox/new",newComponentParams,function(newTextBoxObjectRef) {
+	          console.log("saveNewTextBox: Done getting new ID:response=" + JSON.stringify(newTextBoxObjectRef));
+		  
+			  var fieldName = getFieldRef(newTextBoxObjectRef.properties.fieldID).name
+
+			  var placeholderSelector = '#'+containerParams.containerID
+
+			  $(placeholderSelector).find('label').text(fieldName)
+			  $(placeholderSelector).attr("id",newTextBoxObjectRef.textBoxID)
+		  
+			  // Set up the newly created checkbox for resize, selection, etc.
+			  var componentIDs = { formID: formID, componentID: newTextBoxObjectRef.textBoxID}
+			  initFormComponentDesignBehavior(componentIDs,newTextBoxObjectRef,textBoxDesignFormConfig)
+
+			  // Put a reference to the check box's reference object in the check box's DOM element.
+			  // This reference can be retrieved later for property setting, etc.
+			  setElemObjectRef(newTextBoxObjectRef.textBoxID,newTextBoxObjectRef)
+		
+			  $parentDialog.modal("hide")
+
+	       }) // newLayoutContainer API request
 	}
-		
-	function saveNewTextBox(newTextBoxDialog) {
-		console.log("New textBox: done in dialog")
-		
-
-		var newOrExistingFormInfo = getFormFormInfoByPanelID(newTextBoxDialog,createNewOrExistingFieldDialogPanelID)
-		var doCreateNewField = $(newOrExistingFormInfo.newFieldRadioSelector).prop("checked")
-
-
-		if(doCreateNewField) {
-			console.log("saveNewTextBox: New field selected - not implemented yet")
-			$(newCheckboxDialog).dialog('close');
-		} else {
-			console.log("saveNewTextBox: Existing field selected")
-			console.log("saveNewTextBox: getting field id from field = " + newOrExistingFormInfo.existingFieldSelection)
-			var fieldID = $(newOrExistingFormInfo.existingFieldSelectionSelector).val()			
-			console.log("saveNewTextBox: Selected field ID: " + fieldID)
-			
-			var newTextBoxAPIParams = {
-				fieldParentTableID: designFormContext.tableID,
-				parentFormID: newTextBoxParams.containerParams.parentFormID,
-				geometry: newTextBoxParams.containerParams.geometry,
-				fieldID: fieldID
-			}
-			console.log("saveNewTextBox: API params: " + JSON.stringify(newTextBoxAPIParams))
-			
-			jsonAPIRequest("frm/textBox/new",newTextBoxAPIParams,function(newTextBoxObjectRef) {
-		          console.log("saveNewTextBox: Done getting new ID:response=" + JSON.stringify(newTextBoxObjectRef));
-			  
-				  var fieldName = getFieldRef(newTextBoxObjectRef.properties.fieldID).name
-				  $('#'+newTextBoxParams.placeholderID).find('label').text(fieldName)
-				  $('#'+newTextBoxParams.placeholderID).attr("id",newTextBoxObjectRef.textBoxID)
-			  
-				  // Set up the newly created checkbox for resize, selection, etc.
-				  var componentIDs = { formID: formID, componentID: newTextBoxObjectRef.textBoxID}
-				  initFormComponentDesignBehavior(componentIDs,newTextBoxObjectRef,textBoxDesignFormConfig)
-
-				  // Put a reference to the check box's reference object in the check box's DOM element.
-				  // This reference can be retrieved later for property setting, etc.
-				  setElemObjectRef(newTextBoxObjectRef.textBoxID,newTextBoxObjectRef)
-			
-				  newTextBoxParams.containerCreated = true				  
-					  
-				  newTextBoxParams.dialogBox.dialog("close")
-
-		       }) // newLayoutContainer API request
-			
-			
-		} // Create check box with existing field
-		
-	} // saveNewCheckbox()
 	
-	
-	var newOrExistingFieldPanel = createNewOrExistingFieldPanelConfig({
+	var newFormComponentDialogParams = {
+		elemPrefix: "textBox_",
 		parentTableID: parentTableID,
-		elemPrefix:textBoxElemPrefix,
+		formID: formID,
 		fieldTypes: [fieldTypeText,fieldTypeNumber],
-		doneIfSelectExistingField:true,
-		doneFunc:saveNewTextBox})
-	var newFieldPanel = createNewFieldDialogPanelConfig(textBoxElemPrefix)
+		containerParams: containerParams,
+		createNewFormComponent: createNewTextBox
+	}
 	
-	openWizardDialog({
-		closeFunc: function() {
-			console.log("Close dialog")
-			if(!newTextBoxParams.containerCreated)
-			{
-			  // If the the text box creation is not complete, remove the placeholder
-			  // from the canvas.
-				$('#'+newTextBoxParams.placeholderID).remove()
-			}
-      	},
-		width: 500, height: 500,
-		dialogDivID: textBoxDialogSelector,
-		panels: [newOrExistingFieldPanel,newFieldPanel],
-		progressDivID: dialogProgressSelector,
-	})
-		
+	openNewFormComponentDialog(newFormComponentDialogParams)
+	
+			
 } // newLayoutContainer
 
 function initNewTextBoxDialog() {
--- webui/form/components/textBox/templateFileList.go
+++ webui/form/components/textBox/templateFileList.go
@@ -1,6 +1,5 @@
 package textBox
 
 var TemplateFileList = []string{
-	"static/form/components/textBox/newTextBoxDialog.html",
 	"static/form/components/textBox/include.html",
 	"static/form/components/textBox/properties.html"}
