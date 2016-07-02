fa2ae848315aa542a93ee747ca6b1a80b98b309b
Steve Roehling <sroehling@resultra.com>
2016-07-02 06:05:56 -0700

    Migrate dialog for creating new data picker over to Bootstrap based wizard dialog.

--- webui/form/components/datePicker/newDatePickerDialog.js
+++ webui/form/components/datePicker/newDatePickerDialog.js
@@ -1,96 +1,42 @@
 
-var datePickerDialogSelector = "#newDatePicker"
-
 function openNewDatePickerDialog(formID,parentTableID,containerParams)
 {
-	
-	// Must be the same as designForm.go - this is the common prefix on all DOM element IDs to distinguish
-	// checkbox related elements from other form elements.
-	var datePickerElemPrefix = "datePicker_"
-	var dialogProgressSelector = '#' + datePickerElemPrefix + 'NewFormElemDialogProgress'
-	
-	newDatePickerParams = {
-		containerParams: containerParams,
-		containerCreated: false,
-		placeholderID: containerParams.containerID,
-		dialogBox: $( datePickerDialogSelector )
-	}
 		
-	function saveNewDatePicker(newDatePickerDialog) {
-		console.log("New date picker: done in dialog")
-		
-		var newOrExistingFormInfo = getFormFormInfoByPanelID(newDatePickerDialog,createNewOrExistingFieldDialogPanelID)
-		var doCreateNewField = $(newOrExistingFormInfo.newFieldRadioSelector).prop("checked")
-		
-		
-		if(doCreateNewField) {
-			console.log("saveNewDatePicker: New field selected - not implemented yet")
-			$(newDatePickerDialog).dialog('close');
-		} else {
-			console.log("saveNewDatePicker: Existing field selected")
-			console.log("saveNewDatePicker: getting field id from field = " + newOrExistingFormInfo.existingFieldSelection)
-			var fieldID = $(newOrExistingFormInfo.existingFieldSelectionSelector).val()			
-			console.log("saveNewDatePicker: Selected field ID: " + fieldID)
-			
-			var newDatePickerAPIParams = {
-				fieldParentTableID: designFormContext.tableID,
-				parentFormID: newDatePickerParams.containerParams.parentFormID,
-				geometry: newDatePickerParams.containerParams.geometry,
-				fieldID: fieldID
-			}
-			console.log("saveNewDatePicker: API params: " + JSON.stringify(newDatePickerAPIParams))
-			
-			jsonAPIRequest("frm/datePicker/new",newDatePickerAPIParams,function(newDatePickerObjectRef) {
-		          console.log("saveNewDatePicker: Done getting new ID:response=" + JSON.stringify(newDatePickerObjectRef));
-			  
-			  	  var fieldName = getFieldRef(newDatePickerObjectRef.properties.fieldID).name
-				  $('#'+newDatePickerParams.placeholderID).find('label').text(fieldName)
-				  $('#'+newDatePickerParams.placeholderID).attr("id",newDatePickerObjectRef.datePickerID)
-			  
-				  // Set up the newly created checkbox for resize, selection, etc.
-				  var componentIDs = { formID: formID, componentID:newDatePickerObjectRef.datePickerID }
-				  initFormComponentDesignBehavior(componentIDs,newDatePickerObjectRef,datePickerDesignFormConfig)
-				  
-				  // Put a reference to the check box's reference object in the check box's DOM element.
-				  // This reference can be retrieved later for property setting, etc.
-				  setElemObjectRef(newDatePickerObjectRef.datePickerID,newDatePickerObjectRef)
-				  
-			
-				  newDatePickerParams.containerCreated = true				  
-					  
-				  newDatePickerParams.dialogBox.dialog("close")
+	function createNewDatePicker($parentDialog, newComponentParams) {
+		jsonAPIRequest("frm/datePicker/new",newComponentParams,function(newDatePickerObjectRef) {
+	          console.log("saveNewDatePicker: Done getting new ID:response=" + JSON.stringify(newDatePickerObjectRef));
+		  
+		  	  var fieldName = getFieldRef(newDatePickerObjectRef.properties.fieldID).name
 
-		       }) // newLayoutContainer API request
-			
-			
-		} // Create check box with existing field
-		
-	} // saveNewCheckbox()
-		
-	var newOrExistingFieldPanel = createNewOrExistingFieldPanelConfig({
-		parentTableID: parentTableID,
-		elemPrefix:datePickerElemPrefix,
-		fieldTypes: [fieldTypeTime],
-		doneIfSelectExistingField:true,
-		doneFunc:saveNewDatePicker})
-	var newFieldPanel = createNewFieldDialogPanelConfig(datePickerElemPrefix)
+			  var placeholderSelector = '#'+containerParams.containerID
 	
-	openWizardDialog({
-		closeFunc: function() {
-			console.log("Close dialog")
-			if(!newDatePickerParams.containerCreated)
-			{
-			  // If the the text box creation is not complete, remove the placeholder
-			  // from the canvas.
-				$('#'+newDatePickerParams.placeholderID).remove()
-			}
-      	},
-		width: 500, height: 500,
-		dialogDivID: datePickerDialogSelector,
-		panels: [newOrExistingFieldPanel,newFieldPanel],
-		progressDivID: dialogProgressSelector,
-	})
-		
+			  $(placeholderSelector).find('label').text(fieldName)
+			  $(placeholderSelector).attr("id",newDatePickerObjectRef.datePickerID)
+		  
+			  // Set up the newly created checkbox for resize, selection, etc.
+			  var componentIDs = { formID: formID, componentID:newDatePickerObjectRef.datePickerID }
+			  initFormComponentDesignBehavior(componentIDs,newDatePickerObjectRef,datePickerDesignFormConfig)
+			  
+			  // Put a reference to the check box's reference object in the check box's DOM element.
+			  // This reference can be retrieved later for property setting, etc.
+			  setElemObjectRef(newDatePickerObjectRef.datePickerID,newDatePickerObjectRef)
+			  				  
+			  $parentDialog.modal("hide")
+
+	       }) // newLayoutContainer API request
+	}
+	
+	var newFormComponentDialogParams = {
+		elemPrefix: "datePicker_",
+		parentTableID: parentTableID,
+		formID: formID,
+		fieldTypes: [fieldTypeTime],
+		containerParams: containerParams,
+		createNewFormComponent: createNewDatePicker
+	}
+	
+	openNewFormComponentDialog(newFormComponentDialogParams)
+			
 } // newLayoutContainer
 
 function initNewDatePickerDialog() {
--- webui/form/components/datePicker/templateFileList.go
+++ webui/form/components/datePicker/templateFileList.go
@@ -1,6 +1,5 @@
 package datePicker
 
 var TemplateFileList = []string{
-	"static/form/components/datePicker/newDatePickerDialog.html",
 	"static/form/components/datePicker/include.html",
 	"static/form/components/datePicker/properties.html"}
