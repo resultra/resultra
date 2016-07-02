7238ca4f8f9c383856924362c3f2ed32f423722f
Steve Roehling <sroehling@resultra.com>
2016-07-02 06:37:04 -0700

    Migrate dialog to create html editor to Bootstrap based wizard dialog.

--- webui/form/components/htmlEditor/newHtmlEditorDialog.js
+++ webui/form/components/htmlEditor/newHtmlEditorDialog.js
@@ -1,94 +1,43 @@
 
-var htmlEditorDialogSelector = "#newHtmlEditor"
 
 function openNewHtmlEditorDialog(formID,parentTableID,containerParams)
 {
-	
-	// Must be the same as designForm.go - this is the common prefix on all DOM element IDs to distinguish
-	// checkbox related elements from other form elements.
-	var htmlEditorElemPrefix = "htmlEditor_"
-	var dialogProgressSelector = '#' + htmlEditorElemPrefix + 'NewFormElemDialogProgress'
-	
-	newHtmlEditorParams = {
-		containerParams: containerParams,
-		containerCreated: false,
-		placeholderID: containerParams.containerID,
-		dialogBox: $( htmlEditorDialogSelector )
+				
+	function createNewHtmlEditor($parentDialog, newComponentParams) {
+		jsonAPIRequest("frm/htmlEditor/new",newComponentParams,function(newHtmlEditorObjectRef) {
+	          console.log("saveNewHtmlEditor: Done getting new ID:response=" + JSON.stringify(newHtmlEditorObjectRef));
+		  
+			  var fieldName = getFieldRef(newHtmlEditorObjectRef.properties.fieldID).name
+			  
+			  var placeholderSelector = '#'+containerParams.containerID
+			  
+			  $(placeholderSelector).find('label').text(fieldName)
+			  $(placeholderSelector).attr("id",newHtmlEditorObjectRef.htmlEditorID)
+		  
+			  // Set up the newly created editor for resize, selection, etc.
+			  var componentIDs = { formID: formID, componentID:newHtmlEditorObjectRef.htmlEditorID }
+			  initFormComponentDesignBehavior(componentIDs,newHtmlEditorObjectRef,htmlEditorDesignFormConfig)
+			  
+			  // Put a reference to the check box's reference object in the check box's DOM element.
+			  // This reference can be retrieved later for property setting, etc.
+			  setElemObjectRef(newHtmlEditorObjectRef.htmlEditorID,newHtmlEditorObjectRef)
+			  	
+			  $parentDialog.modal("hide")
+
+	       }) // newLayoutContainer API request
 	}
 		
-	function saveNewHtmlEditor(newHtmlEditorDialog) {
-		console.log("New html editor: done in dialog")
-		
-		var newOrExistingFormInfo = getFormFormInfoByPanelID(newHtmlEditorDialog,createNewOrExistingFieldDialogPanelID)
-		var doCreateNewField = $(newOrExistingFormInfo.newFieldRadioSelector).prop("checked")
-
-		if(doCreateNewField) {
-			console.log("saveNewHtmlEditor: New field selected - not implemented yet")
-			$(newHtmlEditorDialog).dialog('close');
-		} else {
-			console.log("saveNewHtmlEditor: Existing field selected")
-			console.log("saveNewHtmlEditor: getting field id from field = " + newOrExistingFormInfo.existingFieldSelection)
-			var fieldID = $(newOrExistingFormInfo.existingFieldSelectionSelector).val()
-			console.log("saveNewHtmlEditor: Selected field ID: " + fieldID)
-			
-			var newHtmlEditorAPIParams = {
-				fieldParentTableID: designFormContext.tableID,
-				parentFormID: newHtmlEditorParams.containerParams.parentFormID,
-				geometry: newHtmlEditorParams.containerParams.geometry,
-				fieldID: fieldID
-			}
-			console.log("saveNewHtmlEditor: API params: " + JSON.stringify(newHtmlEditorAPIParams))
-			
-			jsonAPIRequest("frm/htmlEditor/new",newHtmlEditorAPIParams,function(newHtmlEditorObjectRef) {
-		          console.log("saveNewHtmlEditor: Done getting new ID:response=" + JSON.stringify(newHtmlEditorObjectRef));
-			  
-				  var fieldName = getFieldRef(newHtmlEditorObjectRef.properties.fieldID).name
-				  $('#'+newHtmlEditorParams.placeholderID).find('label').text(fieldName)
-				  $('#'+newHtmlEditorParams.placeholderID).attr("id",newHtmlEditorObjectRef.htmlEditorID)
-			  
-				  // Set up the newly created editor for resize, selection, etc.
-				  var componentIDs = { formID: formID, componentID:newHtmlEditorObjectRef.htmlEditorID }
-				  initFormComponentDesignBehavior(componentIDs,newHtmlEditorObjectRef,htmlEditorDesignFormConfig)
-				  
-				  // Put a reference to the check box's reference object in the check box's DOM element.
-				  // This reference can be retrieved later for property setting, etc.
-				  setElemObjectRef(newHtmlEditorObjectRef.htmlEditorID,newHtmlEditorObjectRef)
-				  
-			
-				  newHtmlEditorParams.containerCreated = true				  
-					  
-				  newHtmlEditorParams.dialogBox.dialog("close")
-
-		       }) // newLayoutContainer API request
-			
-			
-		} // Create check box with existing field
-		
-	} // saveNewCheckbox()
-		
-	var newOrExistingFieldPanel = createNewOrExistingFieldPanelConfig({
+	var newFormComponentDialogParams = {
+		elemPrefix: "htmlEditor_",
 		parentTableID: parentTableID,
-		elemPrefix:htmlEditorElemPrefix,
+		formID: formID,
 		fieldTypes: [fieldTypeLongText],
-		doneIfSelectExistingField:true,
-		doneFunc:saveNewHtmlEditor})
-	var newFieldPanel = createNewFieldDialogPanelConfig(htmlEditorElemPrefix)
+		containerParams: containerParams,
+		createNewFormComponent: createNewHtmlEditor
+	}
 	
-	openWizardDialog({
-		closeFunc: function() {
-			console.log("Close dialog")
-			if(!newHtmlEditorParams.containerCreated)
-			{
-			  // If the the text box creation is not complete, remove the placeholder
-			  // from the canvas.
-				$('#'+newHtmlEditorParams.placeholderID).remove()
-			}
-      	},
-		width: 500, height: 500,
-		dialogDivID: htmlEditorDialogSelector,
-		panels: [newOrExistingFieldPanel,newFieldPanel],
-		progressDivID: dialogProgressSelector,
-	})
+	openNewFormComponentDialog(newFormComponentDialogParams)
+
 		
 } // newLayoutContainer
 
--- webui/form/components/htmlEditor/templateFileList.go
+++ webui/form/components/htmlEditor/templateFileList.go
@@ -1,6 +1,5 @@
 package htmlEditor
 
 var TemplateFileList = []string{
-	"static/form/components/htmlEditor/newHtmlEditorDialog.html",
 	"static/form/components/htmlEditor/include.html",
 	"static/form/components/htmlEditor/properties.html"}
