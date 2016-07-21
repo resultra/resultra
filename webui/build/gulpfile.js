6e3a9915092f46859cf3439ffa4597b09175761c
Steve Roehling <sroehling@resultra.com>
2016-07-21 15:24:37 -0700

    Add a gulp target to inject minified asset references into HTML files.

--- webui/build/gulpfile.js
+++ webui/build/gulpfile.js
@@ -15,7 +15,7 @@ var cwdAbsPath = __dirname
 gutil.log("Loading asset list from file: " + args.assets)
 var assets = require(args.assets);
 
-gulp.task('debugExportAssets', function() {
+gulp.task('exportIndividualAssets', function() {
 
 	gutil.log("Exporting individual javascript files from asset list: # files = " + assets.jsFiles.length)
 	gulp.src(assets.jsFiles,{base:assets.basePath})
@@ -31,7 +31,7 @@ gulp.task('exportMinifiedAssets', function() {
 	gutil.log("Building minified javascript file from asset list: files = " + assets.jsFiles.length 
 		+ ", target file = " + assets.minJSFile)
 	
-  gulp.src(assets.jsFiles,{base:assets.basePath})
+	gulp.src(assets.jsFiles,{base:assets.basePath})
       .pipe(concat(assets.minJSFile))
       .pipe(uglify())
       .pipe(gulp.dest(distDir))
@@ -39,14 +39,37 @@ gulp.task('exportMinifiedAssets', function() {
   // A single CSS file for release builds
 	gutil.log("Building minified CSS file from asset list: files = " + assets.cssFiles.length 
 		+ ", target file = " + assets.minCSSFile)
-  gulp.src(assets.cssFiles,{base:assets.basePath})
+	gulp.src(assets.cssFiles,{base:assets.basePath})
 	  .pipe(concat(assets.minCSSFile))
 	  .pipe(minifyCSS({keepBreaks:true}))
 	  .pipe(gulp.dest(distDir))
 
 })
 
-gulp.task('injectHTMLFiles', function() {
+// JS file inject is based upon the fully qualified (absolute) file names given 
+// to this task. The transformation simply strips off the basePath of the JS file
+// before returning the HTML reference to be inserted into to the target file.
+// The available gulp-inject options couldn't be made to work right correct prefix
+// to the JS file name.	
+function transformJSFileForInjection(filepath, file, index, length, targetFile) {
+	var newJSPath = '/static' + file.path.replace(assets.basePath,"")
+	gutil.log("transformJSFileForInjection: JS filepath: " + JSON.stringify(file.path) 
+			+ " target HTML file: " + JSON.stringify(targetFile.path))
+	gutil.log("transformJSFileForInjection: transformed path: " + newJSPath)
+	return '<script src="' + newJSPath  + '"></script>'
+}
+
+function transformCSSFileForInjection(filepath, file, index, length, targetFile) {
+	var newCSSPath = '/static' + file.path.replace(assets.basePath,"")
+	gutil.log("transformCSSFileForInjection: CSS filepath: " + JSON.stringify(file.path) 
+			+ " target HTML file: " + JSON.stringify(targetFile.path))
+	gutil.log("transformCSSFileForInjection: transformed path: " + newCSSPath)
+	return '<link rel="stylesheet" href="' + newCSSPath  + '">'
+}
+
+
+
+gulp.task('injectHTMLFilesWithIndividualAssets', function() {
 		
   	// Inject the list of javascript sources into the HTML template(s).
 	// Injection is based upon the absolute path of the JS and HTML file, and uses
@@ -57,57 +80,28 @@ gulp.task('injectHTMLFiles', function() {
 	var cssSources = gulp.src(assets.cssFiles,{read:false})
 
 	gutil.log("Injecting HTML files with JS and CSS references: html files = " + assets.htmlFiles.length 
-		+ ", inject name = " + assets.injectJSName)
-	
-	// JS file inject is based upon the fully qualified (absolute) file names given 
-	// to this task. The transformation simply strips off the basePath of the JS file
-	// before returning the HTML reference to be inserted into to the target file.
-	// The available gulp-inject options couldn't be made to work right correct prefix
-	// to the JS file name.	
-	function transformJSFileForInjection(filepath, file, index, length, targetFile) {
-		var newJSPath = '/static' + file.path.replace(assets.basePath,"")
-		gutil.log("transformJSFileForInjection: JS filepath: " + JSON.stringify(file.path) 
-				+ " target HTML file: " + JSON.stringify(targetFile.path))
-		gutil.log("transformJSFileForInjection: transformed path: " + newJSPath)
-		return '<script src="' + newJSPath  + '"></script>'
-	}
-	
-	function transformCSSFileForInjection(filepath, file, index, length, targetFile) {
-		var newCSSPath = '/static' + file.path.replace(assets.basePath,"")
-		gutil.log("transformCSSFileForInjection: CSS filepath: " + JSON.stringify(file.path) 
-				+ " target HTML file: " + JSON.stringify(targetFile.path))
-		gutil.log("transformCSSFileForInjection: transformed path: " + newCSSPath)
-		return '<link rel="stylesheet" href="' + newCSSPath  + '">'
-	}
+		+ ", inject name = " + assets.injectPlaceholderName)
 	
 	htmlTarget.pipe(inject(jsSources,{name: assets.injectPlaceholderName, transform: transformJSFileForInjection}))
 		.pipe(inject(cssSources,{name: assets.injectPlaceholderName, transform: transformCSSFileForInjection}))
 		.pipe(gulp.dest(distDir))
 });
 
-/*
+gulp.task('injectHTMLFilesWithMinifiedAssets', function() {
 
-gulp.task('designFormRelease', function() {
-  // A single JS files for release builds
-  var minifiedJS = gulp.src(designFormAssets.jsFiles,{base:designFormAssets.basePath})
-      .pipe(concat('form/designForm.min.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest(distDir))
-	
-  var minifiedJSSource = gulp.src(designFormAssets.jsFiles,{base:designFormAssets.basePath})
-      .pipe(concat('form/designForm.min.js'))
-	
-  // A single CSS file for release builds
-  gulp.src(designFormAssets.cssFiles,{base:designFormAssets.basePath})
-	  .pipe(concat('designForm.min.css'))
-	  .pipe(minifyCSS({keepBreaks:true}))
-	  .pipe(gulp.dest(distDir))
-	
-	var target = gulp.src('./designForm.html')
+	var htmlTarget = gulp.src(assets.htmlFiles)
 
-	target.pipe(inject(minifiedJSSource,{name: 'designFormJS',relative:true,addPrefix:'/static/form'}))
+	gutil.log("Injecting HTML files with minified JS and CSS references: html files = " + assets.htmlFiles.length 
+		+ ", inject placholder name = " + assets.injectPlaceholderName)
+
+	var jsSource = gulp.src(assets.jsFiles,{base:assets.basePath})
+      .pipe(concat(assets.minJSFile))
+
+	var cssSource = gulp.src(assets.cssFiles,{base:assets.basePath})
+      .pipe(concat(assets.minCSSFile))
+	
+	htmlTarget.pipe(inject(jsSource,{name: assets.injectPlaceholderName,transform: transformJSFileForInjection}))
+		.pipe(inject(cssSource,{name: assets.injectPlaceholderName, transform: transformCSSFileForInjection}))
 		.pipe(gulp.dest(distDir))
-
+		
 });
-
-*/
