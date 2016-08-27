50a9a1b48a347852159388ddf9aa48a8c2603b4d
Steve Roehling <sroehling@resultra.com>
2016-08-26 20:28:46 -0700

    Finish implementing layout functions for remaining field types and fix vertical alignment of components with rows.

--- webui/form/components/formComponentLoader.js
+++ webui/form/components/formComponentLoader.js
@@ -102,7 +102,7 @@ function loadFormComponents(loadFormConfig) {
 													
 		var compenentIDComponentMap = {}	
 
-		function initTextBox($componentRow,textBox) {
+		function initTextBoxLayout($componentRow,textBox) {
 			// Create an HTML block for the container
 			console.log("loadFormComponents: initializing text box: " + JSON.stringify(textBox))
 		
@@ -126,7 +126,7 @@ function loadFormComponents(loadFormConfig) {
 			
 		}
 
-		function initCheckBox($componentRow,checkBox) {
+		function initCheckBoxLayout($componentRow,checkBox) {
 			// Create an HTML block for the container
 			
 			var containerHTML = checkBoxContainerHTML(checkBox.checkBoxID);
@@ -148,6 +148,74 @@ function loadFormComponents(loadFormConfig) {
 			// Callback for any specific initialization for either the form design or view mode 
 			loadFormConfig.initCheckBoxFunc(checkBox)
 		}
+		
+		function initDatePickerLayout($componentRow,datePicker) {
+			// Create an HTML block for the container			
+			var containerHTML = datePickerContainerHTML(datePicker.datePickerID);
+			var containerObj = $(containerHTML)
+			
+			// Set the label to the field name
+			var fieldName = getFieldRef(datePicker.properties.fieldID).name
+			containerObj.find('label').text(fieldName)
+			
+			// Position the object withing the #layoutCanvas div
+			$componentRow.append(containerObj)
+			setElemDimensions(containerObj,datePicker.properties.geometry)
+			
+			 // Store the newly created object reference in the DOM element. This is needed for follow-on
+			 // property setting, resizing, etc.
+			setElemObjectRef(datePicker.datePickerID,datePicker)
+			
+			// Callback for any specific initialization for either the form design or view mode
+			loadFormConfig.initDatePickerFunc(datePicker)
+			
+		}
+
+		function initHtmlEditorLayout($componentRow,htmlEditor) {
+			
+			var containerHTML = htmlEditorContainerHTML(htmlEditor.htmlEditorID);
+			var containerObj = $(containerHTML)
+			
+			// Set the label to the field name
+			var fieldName = getFieldRef(htmlEditor.properties.fieldID).name
+			containerObj.find('label').text(fieldName)
+			
+			// Position the object withing the #layoutCanvas div
+			$componentRow.append(containerObj)
+			setElemDimensions(containerObj,htmlEditor.properties.geometry)
+			
+			 // Store the newly created object reference in the DOM element. This is needed for follow-on
+			 // property setting, resizing, etc.
+			setElemObjectRef(htmlEditor.htmlEditorID,htmlEditor)
+			
+			// Callback for any specific initialization for either the form design or view mode
+			loadFormConfig.initHtmlEditorFunc(htmlEditor)
+		}
+		
+		function initImageEditorLayout($componentRow,image) {
+			var containerHTML = imageContainerHTML(image.imageID);
+			var containerObj = $(containerHTML)
+			
+			// Position the object withing the #layoutCanvas div
+			$componentRow.append(containerObj)
+			setElemDimensions(containerObj,image.properties.geometry)
+	
+	
+			// Set the label to the field name
+			var labelID = imageContainerLabelIDFromContainerElemID(image.imageID)
+			console.log("loadFormComponents: initializing label: id=" + labelID)
+			var fieldName = getFieldRef(image.properties.fieldID).name
+			$('#'+labelID).text(fieldName)
+			
+			 // Store the newly created object reference in the DOM element. This is needed for follow-on
+			 // property setting, resizing, etc.
+			setElemObjectRef(image.imageID,image)
+			
+			// Callback for any specific initialization for either the form design or view mode
+			loadFormConfig.initImageFunc(image)
+			
+		}
+
 		
 		function populateFormLayout(formLayout, parentLayoutSelector, compenentIDComponentMap) {
 	
@@ -189,14 +257,16 @@ function loadFormComponents(loadFormConfig) {
 	
 		}
 	
-		for (var textBoxIter in formInfo.textBoxes) {
-			
-			var textBoxProps = formInfo.textBoxes[textBoxIter]
-					
+		// Iterate through each type of component and populate a map/dictonary with 
+		// the component ID and method to initialize the compnent. This map is then
+		// referenced when populating the layout to put each component in the right place
+		// on the layout.
+		for (var textBoxIter in formInfo.textBoxes) {			
+			var textBoxProps = formInfo.textBoxes[textBoxIter]			
 			console.log("Form layout: text box component info=" + JSON.stringify(textBoxProps))
 			compenentIDComponentMap[textBoxProps.textBoxID] = {
 				componentInfo: textBoxProps,
-				initFunc: initTextBox
+				initFunc: initTextBoxLayout
 			}			
 
 		} // for each text box
@@ -206,104 +276,44 @@ function loadFormComponents(loadFormConfig) {
 			console.log("loadFormComponents: initializing check box: " + JSON.stringify(checkBoxProps))
 			compenentIDComponentMap[checkBoxProps.checkBoxID] = {
 				componentInfo: checkBoxProps,
-				initFunc: initCheckBox
+				initFunc: initCheckBoxLayout
+			}		
+		}
+		
+		for (var datePickerIter in formInfo.datePickers) {
+			var datePickerProps = formInfo.datePickers[datePickerIter]
+			console.log("loadFormComponents: initializing date picker: " + JSON.stringify(datePickerProps))
+			compenentIDComponentMap[datePickerProps.datePickerID] = {
+				componentInfo: datePickerProps,
+				initFunc: initDatePickerLayout
 			}			
 			
+		}
+
+		for (var htmlEditorIter in formInfo.htmlEditors) {
+			var htmlEditorProps = formInfo.htmlEditors[htmlEditorIter]
+			console.log("loadFormComponents: initializing html editor: " + JSON.stringify(htmlEditorProps))
+
+			compenentIDComponentMap[htmlEditorProps.htmlEditorID] = {
+				componentInfo: htmlEditorProps,
+				initFunc: initHtmlEditorLayout
+			}			
+		}
+	
+		for (var imageIter in formInfo.images) {
+			var imageProps = formInfo.images[imageIter]
+			console.log("loadFormComponents: initializing image editor: " + JSON.stringify(imageProps))
+
+			compenentIDComponentMap[imageProps.imageID] = {
+				componentInfo: imageProps,
+				initFunc: initImageEditorLayout
+			}			
+
 		}
 		
 		var formLayout = formInfo.form.properties.layout
 		populateFormLayout(formLayout,loadFormConfig.formParentElemID,compenentIDComponentMap)
-		
-		
-		
-/*			
-		for (var datePickerIter in formInfo.datePickers) {
-			
-			// Create an HTML block for the container
-			var datePicker = formInfo.datePickers[datePickerIter]
-			console.log("loadFormComponents: initializing date picker: " + JSON.stringify(datePicker))
-			
-			var containerHTML = datePickerContainerHTML(datePicker.datePickerID);
-			var containerObj = $(containerHTML)
-			
-			// Set the label to the field name
-			var fieldName = getFieldRef(datePicker.properties.fieldID).name
-			containerObj.find('label').text(fieldName)
-			
-			// Position the object withing the #layoutCanvas div
-			$(loadFormConfig.formParentElemID).append(containerObj)
-			setElemGeometry(containerObj,datePicker.properties.geometry)
-			
-			 // Store the newly created object reference in the DOM element. This is needed for follow-on
-			 // property setting, resizing, etc.
-			setElemObjectRef(datePicker.datePickerID,datePicker)
-			
-			// Callback for any specific initialization for either the form design or view mode
-			loadFormConfig.initDatePickerFunc(datePicker)
-			
-
-		} // for each date picker
-
-
-		for (var htmlEditorIter in formInfo.htmlEditors) {
-			
-			// Create an HTML block for the container
-			var htmlEditor = formInfo.htmlEditors[htmlEditorIter]
-			console.log("loadFormComponents: initializing html editor: " + JSON.stringify(htmlEditor))
-			
-			var containerHTML = htmlEditorContainerHTML(htmlEditor.htmlEditorID);
-			var containerObj = $(containerHTML)
-			
-			// Set the label to the field name
-			var fieldName = getFieldRef(htmlEditor.properties.fieldID).name
-			containerObj.find('label').text(fieldName)
-			
-			// Position the object withing the #layoutCanvas div
-			$(loadFormConfig.formParentElemID).append(containerObj)
-			setElemGeometry(containerObj,htmlEditor.properties.geometry)
-			
-			 // Store the newly created object reference in the DOM element. This is needed for follow-on
-			 // property setting, resizing, etc.
-			setElemObjectRef(htmlEditor.htmlEditorID,htmlEditor)
-			
-			// Callback for any specific initialization for either the form design or view mode
-			loadFormConfig.initHtmlEditorFunc(htmlEditor)
-			
-
-		} // for each html editor
-
-		for (var imageIter in formInfo.images) {
-			
-			// Create an HTML block for the container
-			var image = formInfo.images[imageIter]
-			console.log("loadFormComponents: initializing image editor: " + JSON.stringify(image))
-			
-			var containerHTML = imageContainerHTML(image.imageID);
-			var containerObj = $(containerHTML)
-			
-			
-			// Position the object withing the #layoutCanvas div
-			$(loadFormConfig.formParentElemID).append(containerObj)
-			setElemGeometry(containerObj,image.properties.geometry)
-	
-	
-			// Set the label to the field name
-			var labelID = imageContainerLabelIDFromContainerElemID(image.imageID)
-			console.log("loadFormComponents: initializing label: id=" + labelID)
-			var fieldName = getFieldRef(image.properties.fieldID).name
-			$('#'+labelID).text(fieldName)
-			
-			 // Store the newly created object reference in the DOM element. This is needed for follow-on
-			 // property setting, resizing, etc.
-			setElemObjectRef(image.imageID,image)
-			
-			// Callback for any specific initialization for either the form design or view mode
-			loadFormConfig.initImageFunc(image)
-			
-
-		} // for each html editor
-	*/	
-		
+				
 		loadFormConfig.doneLoadingFormDataFunc()
 	})
 	
