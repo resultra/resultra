25aef12c9c970d4b1aa48cb770de5546baff7bcc
Steve Roehling <sroehling@resultra.com>
2016-09-11 16:54:22 -0700

    Add pre-processor unit tests for global values.

--- server/calcField/preprocessor_test.go
+++ server/calcField/preprocessor_test.go
@@ -5,9 +5,8 @@ import (
 )
 
 func testOnePreprocess(t *testing.T, inputStr string, fieldReplMap IdentReplacementMap,
+	globalReplMap IdentReplacementMap,
 	expectedOutput string, whatTest string) {
-
-	globalReplMap := IdentReplacementMap{}
 
 	if preprocessedStr, err := preprocessFormulaInput(inputStr, fieldReplMap, globalReplMap); err != nil {
 		t.Error(err)
@@ -21,9 +20,8 @@ func testOnePreprocess(t *testing.T, inputStr string, fieldReplMap IdentReplacem
 	}
 }
 
-func testOnePreprocessFail(t *testing.T, inputStr string, fieldReplMap IdentReplacementMap, whatTest string) {
-
-	globalReplMap := IdentReplacementMap{}
+func testOnePreprocessFail(t *testing.T, inputStr string, fieldReplMap IdentReplacementMap,
+	globalReplMap IdentReplacementMap, whatTest string) {
 
 	if preprocessedStr, err := preprocessFormulaInput(inputStr, fieldReplMap, globalReplMap); err == nil {
 		t.Errorf("testOnePreprocess (fail): Expecting failure, but preprocessing succeeded: %v: input=%v, output=%v",
@@ -37,18 +35,30 @@ func testOnePreprocessFail(t *testing.T, inputStr string, fieldReplMap IdentRepl
 func TestPreprocess(t *testing.T) {
 
 	fieldReplMap := IdentReplacementMap{}
+	globalReplMap := IdentReplacementMap{}
 
 	fieldReplMap["fieldRef1"] = "fieldID1"
 	fieldReplMap["fieldRef2"] = "fieldID2"
 
-	testOnePreprocess(t, "[fieldRef1]", fieldReplMap, "[fieldID1]", "basic field")
-	testOnePreprocess(t, "[ fieldRef2 ]", fieldReplMap, "[ fieldID2 ]", "whitespace between left bracket and field ref name")
-	testOnePreprocess(t, "[fieldRef2] [fieldRef1]", fieldReplMap, "[fieldID2] [fieldID1]", "multiple field references")
-	testOnePreprocess(t, "variable = sum([fieldRef1])", fieldReplMap, "variable = sum([fieldID1])", "field ref mixed in with other syntax")
+	globalReplMap["globalRef1"] = "globalID1"
+	globalReplMap["globalRef2"] = "globalID2"
 
-	testOnePreprocessFail(t, "[fieldRef3]", fieldReplMap, "no field ID in map for fieldRef3")
-	testOnePreprocessFail(t, "[()]", fieldReplMap, "invalid token inside brackets - needs to be an identifyer")
-	testOnePreprocessFail(t, "[]", fieldReplMap, "no token inside brackets - needs to be an identifyer")
-	testOnePreprocessFail(t, "[%]", fieldReplMap, "unrecognized token % - needs to be an identifyer")
+	testOnePreprocess(t, "[fieldRef1]", fieldReplMap, globalReplMap, "[fieldID1]", "basic field")
+	testOnePreprocess(t, "[ fieldRef2 ]", fieldReplMap, globalReplMap, "[ fieldID2 ]", "whitespace between left bracket and field ref name")
+	testOnePreprocess(t, "[fieldRef2] [fieldRef1]", fieldReplMap, globalReplMap, "[fieldID2] [fieldID1]", "multiple field references")
+	testOnePreprocess(t, "variable = sum([fieldRef1])", fieldReplMap, globalReplMap, "variable = sum([fieldID1])", "field ref mixed in with other syntax")
+
+	testOnePreprocessFail(t, "[fieldRef3]", fieldReplMap, globalReplMap, "no field ID in map for fieldRef3")
+	testOnePreprocessFail(t, "[()]", fieldReplMap, globalReplMap, "invalid token inside brackets - needs to be an identifyer")
+	testOnePreprocessFail(t, "[]", fieldReplMap, globalReplMap, "no token inside brackets - needs to be an identifyer")
+	testOnePreprocessFail(t, "[%]", fieldReplMap, globalReplMap, "unrecognized token % - needs to be an identifyer")
+
+	testOnePreprocess(t, "[[globalRef1]]", fieldReplMap, globalReplMap, "[[globalID1]]", "basic global")
+	testOnePreprocess(t, "[[globalRef1]]*[[globalRef2]]", fieldReplMap, globalReplMap, "[[globalID1]]*[[globalID2]]", "basic global - multiple globals")
+	testOnePreprocess(t, "[[ globalRef1 ]]", fieldReplMap, globalReplMap, "[[ globalID1 ]]", "basic global - leading/trailing whitespace OK")
+	testOnePreprocessFail(t, "[[]]", fieldReplMap, globalReplMap, "no token inside brackets - needs to be an identifyer")
+	testOnePreprocessFail(t, "[[25]]", fieldReplMap, globalReplMap, "no identifier token inside brackets - needs to be an identifier")
+	testOnePreprocessFail(t, "[[-32]]", fieldReplMap, globalReplMap, "no identifier token inside brackets - needs to be an identifier")
+	testOnePreprocessFail(t, "[[globalRef3]]", fieldReplMap, globalReplMap, "no global named globalRef3")
 
 }
