8503f29046b0538626e93ac9586abd2427433086
Steve Roehling <sroehling@resultra.com>
2016-09-16 10:55:37 -0700

    Populate summary table with dummied-up HTML

--- webui/dashboard/components/summaryTable/summaryTable.js
+++ webui/dashboard/components/summaryTable/summaryTable.js
@@ -1,13 +1,52 @@
+function summaryTableTableElemID(summaryTableID) {
+	return summaryTableID + "_table"
+}
+
 function summaryTableComponentHTML(summaryTableID) {
 	
+	var tableElemID = summaryTableTableElemID(summaryTableID)
 	// The actual chart is placed inside a "chartWrapper" div. The outer div is used by draggable and resizable to position 
 	// and resize the bar chart within the dashboard canvas. If the chart is placed directly within the out div, there
 	// is a conflict with the Google chart code disabling the resize behavor after the chart is refreshed.
 	var containerHTML = ''+
 	'<div class="layoutContainer dashboardBarChartComponent" id="'+ summaryTableID+'">' +
-		'<table class="table" id="' + summaryTableID+'_table"></table>'+
+		'<table class="table table-hover table-bordered" id="' + tableElemID+'"></table>'+
 	'</div>';
 	return containerHTML
+}
+
+function populateSummaryTableHeader($summaryTable,summaryTableData) {
+	
+	var $tableHeader = $("<thead></thead>")
+	var $headerRow = $("<tr></tr>")
+	
+	
+	$headerRow.append("<th>Row Header</th>")
+	$headerRow.append("<th>Summary Col 1</th>")
+	
+	$tableHeader.append($headerRow)
+	$summaryTable.append($tableHeader)
+}
+
+function populateSummaryTableRow($tableBody) {
+	
+	var $tableRow = $("<tr></tr>")
+	$tableRow.append("<td><strong>Row Val<strong></td>")
+	$tableRow.append("<td>Col Val</td>")	
+	$tableBody.append($tableRow)
+}
+
+
+function populateSummaryTableRows($summaryTable,summaryTableData) {
+	
+	var $tableBody = $("<tbody></tbody>")
+	
+	populateSummaryTableRow($tableBody)
+	populateSummaryTableRow($tableBody)
+	populateSummaryTableRow($tableBody)
+	
+	$summaryTable.append($tableBody)
+	
 }
 
 
@@ -15,9 +54,14 @@ function initSummaryTableData(dashboardID,summaryTableData) {
 	
 //	drawBarChart(barChartData)
 //	initBarChartEditBehavior(dashboardID, barChartData.barChart.barChartID)
-	
-	var $summaryTable = $('#'+summaryTableData.summaryTable.summaryTableID)
+	var tableElemID = summaryTableTableElemID(summaryTableData.summaryTable.summaryTableID)
+	var $summaryTable = $('#'+tableElemID)
+
+	populateSummaryTableHeader($summaryTable,summaryTableData)
+	populateSummaryTableRows($summaryTable,summaryTableData)
 	
 	$summaryTable.data("summaryTableRef",summaryTableData.summaryTable)
+	
+	
 	
 }
