cc6deb13ce0e949e70a18d67770eb16427540fce
Steve Roehling <sroehling@resultra.com>
2016-09-26 09:08:59 -0700

    Remove superfluous comments from summary table code.

--- webui/dashboard/components/summaryTable/summaryTable.js
+++ webui/dashboard/components/summaryTable/summaryTable.js
@@ -5,9 +5,6 @@ function summaryTableTableElemID(summaryTableID) {
 function summaryTableComponentHTML(summaryTableID) {
 	
 	var tableElemID = summaryTableTableElemID(summaryTableID)
-	// The actual chart is placed inside a "chartWrapper" div. The outer div is used by draggable and resizable to position 
-	// and resize the bar chart within the dashboard canvas. If the chart is placed directly within the out div, there
-	// is a conflict with the Google chart code disabling the resize behavor after the chart is refreshed.
 	var containerHTML = ''+
 	'<div class="layoutContainer dashboardBarChartComponent" id="'+ summaryTableID+'">' +
 		'<table class="table table-hover table-bordered" id="' + tableElemID+'"></table>'+
@@ -63,8 +60,6 @@ function populateSummaryTableRows($summaryTable,summaryTableData) {
 
 function initSummaryTableData(dashboardID,summaryTableData) {
 	
-//	drawBarChart(barChartData)
-//	initBarChartEditBehavior(dashboardID, barChartData.barChart.barChartID)
 	var tableElemID = summaryTableTableElemID(summaryTableData.summaryTable.summaryTableID)
 	var $summaryTable = $('#'+tableElemID)
 
