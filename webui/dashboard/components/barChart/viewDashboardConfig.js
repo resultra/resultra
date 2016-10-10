f0d8b29d66dbdaddd1401c8b7b1813090e2ce097
Steve Roehling <sroehling@resultra.com>
2016-10-10 14:50:26 -0700

    Initialize the filter properties when viewing bar charts.

--- webui/dashboard/components/barChart/viewDashboardConfig.js
+++ webui/dashboard/components/barChart/viewDashboardConfig.js
@@ -1,8 +1,26 @@
 function barChartViewDashboardConfig() {
 	
+	var barChartElemPrefix = "barChart_"
+	
+	function reloadBarChart() {
+		
+	}
+	
 	function selectBarChart(barChartRef) {
 			console.log("Select bar chart: " + barChartRef.barChartID)
 			// Toggle to the summary properties, hiding the other property panels
+		
+		
+			var filterPaneParams = {
+				elemPrefix: barChartElemPrefix,
+				tableID: barChartRef.properties.dataSrcTableID,
+				defaultFilterIDs: barChartRef.properties.defaultFilterIDs,
+				availableFilterIDs: barChartRef.properties.availableFilterIDs,
+				refilterCallbackFunc: reloadBarChart
+			}
+
+			initRecordFilterPanel(filterPaneParams)
+		
 			hideSiblingsShowOne('#barChartViewProps')
 	}
 	
