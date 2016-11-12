4af8db79e6a161cbfd3ec7f6dd1ba205e17719c8
Steve Roehling <sroehling@resultra.com>
2016-11-12 10:32:51 -0700

    Initial integration of a date range filter rule.

--- webui/common/recordFilter/filterRules.js
+++ webui/common/recordFilter/filterRules.js
@@ -34,6 +34,96 @@ function createFilterListRuleListItem(fieldName) {
 	return $filterRuleListItem
 }
 
+function dateFilterPanelRuleItem(panelParams,fieldInfo) {
+	
+	var $ruleControls = $('#recordFilterDateFieldRuleListItem').clone()
+	$ruleControls.attr("id","")
+	
+	var $startEndDateControls = $ruleControls.find(".filterCustomStartEndDateControls")
+	$startEndDateControls.hide()
+	
+	var $dateFilterModeSelection = $ruleControls.find(".filterDateRuleModeSelection")
+	$dateFilterModeSelection.empty()
+	$dateFilterModeSelection.append(defaultSelectOptionPromptHTML("Filter for"))
+	
+	var dateFilterModes = {
+		"any": {
+			label: "Any date",
+			modeSelected: function() {
+				$startEndDateControls.hide()
+				updateFilterRules(panelParams)
+			},
+			modeConfig: function() {
+				var ruleConfig = { fieldID: fieldInfo.fieldID, 
+					ruleID: "anyDate", 
+					param: "" }
+				return ruleConfig				
+			}
+		},
+		"customRange": {
+			label: "Custom date range",
+			modeSelected: function() {
+				$startEndDateControls.show()
+				updateFilterRules(panelParams)
+			},
+			modeConfig: function() {
+				var ruleConfig = { fieldID: fieldInfo.fieldID, 
+					ruleID: "dateRange", 
+					param: "" }
+				return ruleConfig
+			}
+		},
+	}
+	for(var modeID in dateFilterModes) {
+	 	var selectModeHTML = selectOptionHTML(modeID, dateFilterModes[modeID].label)
+	 	$dateFilterModeSelection.append(selectModeHTML)				
+	}
+	initSelectControlChangeHandler($dateFilterModeSelection,function(modeID) {
+		var modeInfo = dateFilterModes[modeID]
+		console.log("Date rule mode selection change: " + modeID)
+		modeInfo.modeSelected()
+	})
+	
+	// Initialize the start and end date controls
+	var $startDatePicker = $ruleControls.find(".filterDateRangeStartInput")
+	$startDatePicker.datetimepicker()
+	
+	var $endDatePicker = $ruleControls.find(".filterDateRangeEndInput")
+	$endDatePicker.datetimepicker({
+            useCurrent: false //Important! See issue #1075
+        });
+	
+	// Link the start and end date controls based to ensure
+    // the range is preserved.
+    $startDatePicker.on("dp.change", function (e) {
+		console.log("Custom start date changed: " + e.date)
+        $endDatePicker.data("DateTimePicker").minDate(e.date);
+		updateFilterRules(panelParams)
+    });
+    $endDatePicker.on("dp.change", function (e) {
+		console.log("Custom end date changed: " + e.date)
+        $startDatePicker.data("DateTimePicker").maxDate(e.date);
+		updateFilterRules(panelParams)
+    });
+	
+	var $filterListItem = createFilterListRuleListItem(fieldInfo.name)
+	$filterListItem.data("filterRuleConfigFunc",function() {
+		console.log("Date filter rule - getting config")
+		var modeID = $dateFilterModeSelection.val()
+		if(modeID !== null && modeID.length > 0) {
+			var modeInfo = dateFilterModes[modeID]
+			return modeInfo.modeConfig()
+		} else {
+			return null
+		}
+		
+	})
+	$filterListItem.append($ruleControls)
+		
+	return $filterListItem
+	
+	
+}
 
 
 function numberFilterPanelRuleItem(panelParams, fieldInfo) {
@@ -106,6 +196,8 @@ function createFilterRulePanelListItem(panelParams, fieldInfo) {
 	switch (fieldInfo.type) {
 	case fieldTypeNumber:
 		return numberFilterPanelRuleItem(panelParams, fieldInfo)
+	case fieldTypeTime: 
+		return dateFilterPanelRuleItem(panelParams, fieldInfo)
 	default:
 		console.log("createFilterRulePanelListItem: Unsupported field type:  " + fieldInfo.type)
 		return $("")
