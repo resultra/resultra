c0485da6d1a07b03009fc9cb2d1af8bb153a4782
Steve Roehling <sroehling@resultra.com>
2017-01-17 14:51:03 -0700

    Add a dialog box to select the form to be linked to button.

--- webui/common/form/components/formButton/newFormButtonDialog.js
+++ webui/common/form/components/formButton/newFormButtonDialog.js
@@ -1,30 +1,81 @@
 function openNewFormButtonDialog(databaseID,formID,containerParams) {
 	console.log("New form button dialog")
 	
-	var newButtonParams = {
-		parentFormID: formID,
-		geometry: containerParams.geometry}
+		var $newFormButtonDialogForm = $('#newFormButtonDialogForm')
 	
-	
-	jsonAPIRequest("frm/formButton/new",newButtonParams,function(newButtonObjectRef) {
+		var validator = $newFormButtonDialogForm.validate({
+			rules: {
+				newFormButtonFormLinkSelection: {
+					required: true,
+				}, // newFormButtonFormLinkSelection
+			},
+			messages: {
+				newFormButtonFormLinkSelection: {
+					required: "Selection of a popup form is required"
+				}
+			}
+		})
+
+		validator.resetForm()
 		
-          console.log("create new form button: Done getting new ID:response=" + JSON.stringify(newButtonObjectRef));
-    			  
-		  var placeholderSelector = '#'+containerParams.containerID
-		  
-		  var buttonLabel = "TBD - Button Label"
-		  $(placeholderSelector).find('.formButton').text(buttonLabel)
-		  $(placeholderSelector).attr("id",newButtonObjectRef.buttonID)
-  
-		  // Set up the newly created checkbox for resize, selection, etc.
-		  var componentIDs = { formID: formID, componentID:newButtonObjectRef.buttonID }
-		  
-		  initFormComponentDesignBehavior(componentIDs,newButtonObjectRef,formButtonDesignFormConfig)
+		var selectFormParams = {
+			menuSelector: "#newFormButtonFormLinkSelection",
+			parentDatabaseID: databaseID
+		}	
+		populateFormSelectionMenu(selectFormParams)
+		
+		var $newFormButtonDialog = $('#newFormButtonDialog')
+		var placeholderSelector = '#'+containerParams.containerID
+		
+		var componentCreated = false
+		$newFormButtonDialog.modal('show')
+	
+		initButtonClickHandler('#newFormButtonSaveButton',function() {
+			console.log("New form button save button clicked")
+			if($newFormButtonDialogForm.valid()) {	
+				
+				var newButtonParams = {
+					parentFormID: formID,
+					geometry: containerParams.geometry,
+					linkedFormID: $("#newFormButtonFormLinkSelection").val() }
+				
+				jsonAPIRequest("frm/formButton/new",newButtonParams,function(newButtonObjectRef) {
+	
+			          console.log("create new form button: Done getting new ID:response=" + JSON.stringify(newButtonObjectRef));
+			  
+					  var buttonLabel = "TBD - Button Label"
+					  $(placeholderSelector).find('.formButton').text(buttonLabel)
+					  $(placeholderSelector).attr("id",newButtonObjectRef.buttonID)
+
+					  // Set up the newly created checkbox for resize, selection, etc.
+					  var componentIDs = { formID: formID, componentID:newButtonObjectRef.buttonID }
 	  
-		  // Put a reference to the check box's reference object in the check box's DOM element.
-		  // This reference can be retrieved later for property setting, etc.
-		  setElemObjectRef(newButtonObjectRef.buttonID,newButtonObjectRef)
-	  			  
-       }) // newLayoutContainer API request
+					  initFormComponentDesignBehavior(componentIDs,newButtonObjectRef,formButtonDesignFormConfig)
+  
+					  // Put a reference to the check box's reference object in the check box's DOM element.
+					  // This reference can be retrieved later for property setting, etc.
+					  setElemObjectRef(newButtonObjectRef.buttonID,newButtonObjectRef)
+					  
+					  componentCreated = true
+					  $newFormButtonDialog.modal('hide')
+  			  
+			    }) // newLayoutContainer API request
+			
+			}
+		})
+		
+		$newFormButtonDialog.unbind("hidden.bs.modal")
+		$newFormButtonDialog.on("hidden.bs.modal", function () {
+		    // put your default event here
+			if(!componentCreated) {
+				console.log("Cancel new button component creation: removing placholder component = " 
+									+ placeholderSelector)
+				$(placeholderSelector).remove()				
+			}
+		});
+		
+		
+	
+	
 	
 }
--- webui/common/form/components/formButton/templateFileList.go
+++ webui/common/form/components/formButton/templateFileList.go
@@ -1,3 +1,4 @@
 package formButton
 
-var TemplateFileList = []string{"static/common/form/components/formButton/properties.html"}
+var TemplateFileList = []string{"static/common/form/components/formButton/properties.html",
+	"static/common/form/components/formButton/newFormButtonDialog.html"}
