3f2bd67c9e148aaeafd79caa37afbc5942b930ff
Steve Roehling <sroehling@resultra.com>
2017-03-02 11:01:29 -0700

    Fix the asset building process for shared user sign-in page so the page with its injected CSS/JS asset lists doesn't get "clobbered" by other directories including the package.
    Accomplished by introducing a hand-coded AssetManifest.json without the HTML template file for the sign-in page.

--- webui/build/webui.mk
+++ webui/build/webui.mk
@@ -10,6 +10,11 @@ GEN_ASSET_MANIFEST = $(GEN_ASSET_MANFIEST) ./assetManifest.json $(ASSET_BASE_PAT
 
 GULP = gulp --gulpfile $(DEPTH)/webui/build/gulpfile.js
 
+# TODO - the EXPORT_ASSETS rule will export *all* the assets found in assetManifest_gen.json
+# However, many times an included asset from another package/directory will be exported multiple times
+# if it is included from another directory. This means a single included asset may be exported many times,
+# and possibily erroneously exported without its own injected asset lists included. There needs to be
+# an enhancment to the overall build system to prevent assets from being exported more than once.
 
 ifeq ($(DEBUG),1)
 	EXPORT_ASSETS = $(GULP) exportIndividualAssets  --assets $(abspath ./assetManifest_gen.json)
