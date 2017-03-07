a58a3274677c608793f52fee6e4df98138ac6903
Steve Roehling <sroehling@resultra.com>
2017-03-07 14:03:55 -0700

    Continue integration of Bootstrap date component - expand settings to include explicit defaults and start building support for settings given in the property panel.

--- webui/common/form/components/datePicker/viewFormConfig.js
+++ webui/common/form/components/datePicker/viewFormConfig.js
@@ -1,3 +1,14 @@
+
+function getDateComponentDateTimeFormat() {
+	// Populate with multiple formats which are selectable as an option for this component.
+	var formatMap = {
+		"dateOnly": 'MM/DD/YYYY',
+		"dateTime": 'YYYY-MM-DD hh:mma' // Date with 12 hour time
+	}
+	var dateTimeFormat = formatMap['dateTime']
+	return dateTimeFormat
+}
+
 function loadRecordIntoDatePicker($datePicker, recordRef) {
 	
 	console.log("loadRecordIntoDatePicker: loading record into date picker: " + JSON.stringify(recordRef))
@@ -18,7 +29,7 @@ function loadRecordIntoDatePicker($datePicker, recordRef) {
 	
 		// The jQuery UI date picker only supports dates. So, until the Bootstrap datetime picker can be
 		// integrated, only the date will be formatted and shown in the input field.
-		var dateVal = moment(fieldVal).format("MM/DD/YYYY")
+		var dateVal = moment(fieldVal).format(getDateComponentDateTimeFormat())
 
 		var currDateVal = $datePickerInput.val()
 	
@@ -62,32 +73,38 @@ function initDatePickerFieldEditBehavior(componentContext,recordProxy, datePicke
 	    console.log("date picker changed dates")
 		// Get the most recent copy of the object reference. It could have changed between
 		// initialization time and the time the checkbox was changed.
-		var objectRef = getContainerObjectRef($datePickerContainer)
+		
+		// The date passed with the event will be false if the date has been cleared.
+		if(e.date !== false) {
+			var objectRef = getContainerObjectRef($datePickerContainer)
 				
-		var dateParam = e.date.toISOString()
+			var dateParam = e.date.toISOString()
 		
-		currRecordRef = recordProxy.getRecordFunc()
+			currRecordRef = recordProxy.getRecordFunc()
 		
-		var dateValueFormat = {
-			context: "datePicker",
-			format: "date"
-		}
-		var setRecordValParams = {
-			parentDatabaseID:currRecordRef.parentDatabaseID,
-			recordID:currRecordRef.recordID, 
-			changeSetID: recordProxy.changeSetID,
-			fieldID:fieldID, 
-			value:dateParam,
-			 valueFormat: dateValueFormat}
-		console.log("Setting date value: " + JSON.stringify(setRecordValParams))
+			var dateValueFormat = {
+				context: "datePicker",
+				format: "date"
+			}
+			var setRecordValParams = {
+				parentDatabaseID:currRecordRef.parentDatabaseID,
+				recordID:currRecordRef.recordID, 
+				changeSetID: recordProxy.changeSetID,
+				fieldID:fieldID, 
+				value:dateParam,
+				 valueFormat: dateValueFormat}
+			console.log("Setting date value: " + JSON.stringify(setRecordValParams))
 		
-		jsonAPIRequest("recordUpdate/setTimeFieldValue",setRecordValParams,function(updatedRecordRef) {
+			jsonAPIRequest("recordUpdate/setTimeFieldValue",setRecordValParams,function(updatedRecordRef) {
 			
-			// After updating the record, the local cache of records in currentRecordSet will
-			// be out of date. So after updating the record on the server, the locally cached
-			// version of the record also needs to be updated.
-			recordProxy.updateRecordFunc(updatedRecordRef)
-		}) // set record's text field value
+				// After updating the record, the local cache of records in currentRecordSet will
+				// be out of date. So after updating the record on the server, the locally cached
+				// version of the record also needs to be updated.
+				recordProxy.updateRecordFunc(updatedRecordRef)
+			}) // set record's text field value
+			
+		}
+		
 		
 	});	
 	
@@ -96,9 +113,22 @@ function initDatePickerFieldEditBehavior(componentContext,recordProxy, datePicke
 
 function initDatePickerRecordEditBehavior($datePickerContainer, componentContext,recordProxy, datePickerObjectRef) {
 		
+	var dateTimeFormat = getDateComponentDateTimeFormat()
+		
+		
 	var $datePickerInput = datePickerInputFromContainer($datePickerContainer)
 	$datePickerInput.datetimepicker({
-		format: 'MM/DD/YYYY' // can be any moment format
+		format: dateTimeFormat, // moment format - time will be displayed if the format includes a time component
+		showTodayButton: true, // show the today button as a default
+		keepOpen: false, // keep the popup open after selecting a date
+	    daysOfWeekDisabled: [0,6], // Disable weekends
+		showClear:true, // Show the button to clear the date (trash icon)
+		sideBySide: false, // When editing both date and time, show the time side-by-side with the date.
+		inline: false, // Display the date & time picker inline without the need to of an input field
+		stepping: 5, // number of minutes the up and down arrows will step in clicking the up and down arrows.
+		useCurrent: false, // When the date picker is shown, set the picker to the current date/time
+		// daysOfWeekHighlighted: "1,2,3,4,5" // highlight weekdays
+		// 
 	})
 	
 	$datePickerContainer.data("viewFormConfig", {
