48956775a940925fb5e1438731a7e18ea51ca9f8
Steve Roehling <sroehling@resultra.com>
2017-03-09 08:26:38 -0700

    Adjust the markup of checkboxes to include a unique ID to associated the checkbox with its label.

--- webui/common/form/components/checkBox/containerHTML.js
+++ webui/common/form/components/checkBox/containerHTML.js
@@ -7,14 +7,25 @@ function getCheckboxControlFromCheckboxContainer($checkboxContainer) {
 
 
 
+// Support the generation of unique IDs for each individual checkbox. This isn't used
+// to idenfity which field the checkbox is connected to, but to connect the checkbox
+// to its label, so clicking on the label will check/uncheck the checkbox as well.
+var uniqueCheckboxIDForLabel = 1
+function generateUniqueCheckboxIDForLabel() {
+	uniqueCheckboxIDForLabel++
+	return "checkboxComponent_" + uniqueCheckboxIDForLabel
+}
 
 function checkBoxContainerHTML(elementID)
 {	
+	
+	var uniqueID = generateUniqueCheckboxIDForLabel()
+	
 	var containerHTML = ''+
 		'<div class=" layoutContainer checkBoxFormContainer">' +
 			'<div class="checkbox">' +
-				'<input type="checkbox" class="checkboxFormComponentControl">' +
-				'<label class="checkboxFormComponentLabel"></label>' + 
+				'<input type="checkbox" id="'+uniqueID+'"class="checkboxFormComponentControl">' +
+				'<label for="'+  uniqueID + '"class="checkboxFormComponentLabel"></label>' + 
 			'</div>' +
 		'</div><';
 				
