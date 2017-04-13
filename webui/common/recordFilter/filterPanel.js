b8db8b4e54fd8232cb9c3a294347f96a103899fe
Steve Roehling <sroehling@resultra.com>
2017-04-12 17:05:09 -0700

    Clean up the formatting of the record filtering pane for the item list and dashboard components.

--- webui/common/recordFilter/filterPanel.js
+++ webui/common/recordFilter/filterPanel.js
@@ -1,18 +1,13 @@
 
 function initRecordFilterViewPanel(filterPaneParams) {
 		
-	initDefaultFilterRules(filterPaneParams) 
-		
-	var refilterButtonSelector = createPrefixedSelector(filterPaneParams.elemPrefix,'FilterRecordsPanelRefilterButton')
-	initButtonClickHandler(refilterButtonSelector,function (){
-		filterPaneParams.refilterWithCurrentFilterRules()		
-	})
-	
+	initDefaultFilterRules(filterPaneParams) 	
 	
 	var resetFiltersButtonSelector = createPrefixedSelector(filterPaneParams.elemPrefix,'FilterRecordsResetFiltersButton')
 	initButtonClickHandler(resetFiltersButtonSelector,function () {
 		updateDefaultFilterRules(filterPaneParams, function () {
 			filterPaneParams.updateFilterRules(filterPaneParams.defaultFilterRules)
+			filterPaneParams.refilterWithCurrentFilterRules()		
 		})		
 	})
 	
