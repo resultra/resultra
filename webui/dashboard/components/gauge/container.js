f7d120c4400805b2470c3a3d94817c652dd85171
Steve Roehling <sroehling@resultra.com>
2017-05-03 16:33:23 -0700

    Initial integration of the gauge control with the dashboard component.

--- webui/dashboard/components/gauge/container.js
+++ webui/dashboard/components/gauge/container.js
@@ -7,19 +7,62 @@ function dashboardGaugeContainerHTML(elementID)
 {	
 	var containerHTML = ''+
 		'<div class="layoutContainer dashboardGaugeFormContainer" id="'+elementID+'">' +
-			'<span class="h3 dashboardGauge">' +
-			'New Gauge' +
-			'</span>' +
+			'<label class="gaugeLabel">' + 'New Gauge' + '</label>' +
+			'<div class="dashboardGauge">' +
+				'<span class="gaugeControl"></span>'+
+  			'</div>' +
+	
 		'</div><';
 						
 	return containerHTML
 }
 
 function setGaugeDashboardComponentLabel($container,gaugeRef) {
-	var $label = $container.find("span")
+	var $label = $container.find(".gaugeLabel")
 	$label.text(gaugeRef.properties.title)	
+}
+
+function initGaugeDashboardComponentControl($gauge,gaugeConfig,gaugeVal) {
+	var $gaugeControlContainer = $gauge.find(".gaugeControl")
+	var gaugeControl = new GaugeUIControl($gaugeControlContainer, gaugeConfig);
+	gaugeControl.render()
+	gaugeControl.redraw(gaugeConfig.min)
+	$gauge.data("gaugeControl",gaugeControl) 
+	
+	gaugeControl.redraw(gaugeVal)
+	
 }
 
 
 function initGaugeData(dashboardID,$gauge, gaugeData) {
+	
+	var gaugeRef = gaugeData.gauge	
+	var gaugeVal = gaugeData.groupedSummarizedVals.overallDataRow.summaryVals[0]
+	
+	function formatGaugeVal(val) {
+		var numberFormat = "general"
+//		var numberFormat = gaugeRef.properties.valueFormat.format
+		var formattedVal = formatNumberValue(numberFormat,val)
+		return formattedVal
+	}
+	
+//	var minVal = gaugeRef.properties.minVal
+	var minVal = 0
+// var maxVal = gaugeRef.properties.maxVal
+	var maxVal = 100
+		
+	var gaugeConfig = 
+	{
+		size: gaugeRef.properties.geometry.sizeWidth,
+		min: minVal,
+		max: maxVal,
+		minorTicks: 5,
+		valueFormatter: formatGaugeVal
+	}	
+	gaugeConfig.yellowZones = [];
+	gaugeConfig.redZones = [];
+	gaugeConfig.greenZones = [];
+	
+	initGaugeDashboardComponentControl($gauge,gaugeConfig,gaugeVal)
+	
 }
