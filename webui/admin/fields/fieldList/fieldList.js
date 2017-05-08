28e55faf9a0fba2efb9f09d11422431986b6b731
Steve Roehling <sroehling@resultra.com>
2017-05-08 11:01:15 -0700

    Initial display of fields in the admin page using a DataTable.

--- webui/admin/fields/fieldList/fieldList.js
+++ webui/admin/fields/fieldList/fieldList.js
@@ -3,31 +3,42 @@ function initAdminFieldSettings(databaseID) {
 		
 	function addFieldToAdminFieldList(fieldInfo) {
  
-		var $fieldListItem = $('#adminFieldItemTemplate').clone()
-		$fieldListItem.attr("id","")
+		var $fieldListRow = $('#adminFieldItemRowTemplate').clone()
+		$fieldListRow.attr("id","")
 	
-		$fieldListItem.attr("data-fieldID",fieldInfo.fieldID)
-	
-		var $editFieldPropsButton = $fieldListItem.find(".editFieldPropsButton")
+		$fieldListRow.attr("data-fieldID",fieldInfo.fieldID)
+		
+		var $editFieldPropsButton = $fieldListRow.find(".editFieldPropsButton")
 		var editPropsLink = '/admin/field/' + fieldInfo.fieldID
 		$editFieldPropsButton.attr("href",editPropsLink)
 
-		var $nameLabel = $fieldListItem.find(".fieldNameLabel")
-		$nameLabel.text(fieldInfo.name)
+		var $fieldNameCol = $fieldListRow.find(".fieldNameCol")
+		$fieldNameCol.text(fieldInfo.name)
 		
+		console.log("field row info: " + $fieldListRow.html())
 	
-		$('#adminFieldList').append($fieldListItem)		
+		$('#adminFieldListBody').append($fieldListRow)		
 	}
 		
 	
-	loadFieldInfo(databaseID, [fieldTypeAll],function(fieldsByID) {
-		for (var fieldID in fieldsByID) {
-	
-			var fieldInfo = fieldsByID[fieldID]	
-			
-			addFieldToAdminFieldList(fieldInfo)	
-	
+	loadSortedFieldInfo(databaseID, [fieldTypeAll],function(sortedFields) {
+		for (var fieldIndex in sortedFields) {
+			var fieldInfo = sortedFields[fieldIndex]	
+			addFieldToAdminFieldList(fieldInfo)		
 		} // for each  field
+		$('#adminFieldList').DataTable({
+			destroy:true, // Destroy existing table before applying the options
+			searching:false, // Hide the search box
+			bInfo:false, // Hide the "Showing 1 of N Entries" below the footer
+			paging:false,
+			//scrollY: '500px',
+			//scrollCollapse:true,
+		
+			aoColumnDefs: [
+			          { bSortable: false, aTargets: [ 1 ] }
+			       ]
+		
+		})
 	})
 	
 	initButtonClickHandler('#adminNewFieldButton',function() {
