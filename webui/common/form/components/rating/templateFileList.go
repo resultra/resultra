0bf0c1e9b3cbbd7c7edb87e6b845efdf0e688525
Steve Roehling <sroehling@resultra.com>
2017-06-12 15:18:09 -0700

    Integrate local versions of jQuery, Bootstrap and Datatables libraries.

--- webui/common/form/components/rating/templateFileList.go
+++ webui/common/form/components/rating/templateFileList.go
@@ -1,5 +1,4 @@
 package rating
 
 var TemplateFileList = []string{"static/common/form/components/rating/properties.html",
-	"static/common/form/components/rating/tooltipProperties.html",
 	"static/common/form/components/rating/viewProperties.html"}
