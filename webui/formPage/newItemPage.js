9f83ca0d13e6cb8a019d999a573fe7381a0b2d53
Steve Roehling <sroehling@resultra.com>
2017-06-14 14:27:19 -0700

    Include standard site navigation elements in form to create new items.

--- webui/formPage/newItemPage.js
+++ webui/formPage/newItemPage.js
@@ -1,8 +1,46 @@
+function initNewItemPageLayout()
+{
+	var zeroPaddingInset = { top:0, bottom:0, left:0, right:0 }
+	
+	// Initialize the page layout
+	var mainLayout = $('#layoutPage').layout({
+		inset: zeroPaddingInset,
+		north: fixedUILayoutPaneParams(40),
+		west: {
+			size: 250,
+			resizable:false,
+			slidable: false,
+			spacing_open:4,
+			spacing_closed:4,
+			initClosed:true // panel is initially closed	
+		}
+	})
+		
+	var formLayout = $('#newItemFormPane').layout({
+		north: fixedUILayoutPaneAutoSizeToFitContentsParams(),
+		north__showOverflowOnHover:	true
+	})
+	
+	initButtonClickHandler("#viewTableOfContentsMenuButton", function() {
+		mainLayout.toggle("west")
+	})
+		
+}
+
+
+
 $(document).ready(function() {	
 	 
-	initSubmitFormUILayoutPanes()
+	initNewItemPageLayout()
 				
 	initUserDropdownMenu()
+	
+	var tocConfig = {
+		databaseID: submitFormPageContext.databaseID,
+		newItemFormButtonFunc: openSubmitFormDialog
+	}	
+	initDatabaseTOC(tocConfig)
+	
 	
 	var submitFormParams = {
 		databaseID: submitFormPageContext.databaseID,
