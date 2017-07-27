77ba23eefc3e10f6f75a6a8a63330911388cc595
Steve Roehling <sroehling@resultra.com>
2017-07-27 15:43:34 -0700

    Add role list to collaborator properties page.

--- webui/admin/collaborators/collaboratorProps/collaboratorProps.js
+++ webui/admin/collaborators/collaboratorProps/collaboratorProps.js
@@ -3,5 +3,43 @@ $(document).ready(function() {
 	initAdminSettingsPageLayout($('#collabPropsPage'))	
 	initUserDropdownMenu()
 	initAdminSettingsTOC(collabPropsContext.databaseID)
+	
+	
+	function addRoleToRoleCheckboxList(roleInfo) {
+			
+		var roleCheckboxHTML = '' +
+			'<div class="list-group-item">' +
+				'<div class="checkbox">' +
+					'<input type="checkbox" id="' + roleInfo.roleID + '"></input>'+
+					'<label for="' + roleInfo.roleID +  '"><span class="noselect roleNameLabel"></span></label>' +
+				'</div>' + 
+			'</div>'
+		
+		var $roleCheckbox = $(roleCheckboxHTML)
+		$roleCheckbox.find('.roleNameLabel').text(roleInfo.roleName)
+		
+	
+		$('#adminCollabRolesList').append($roleCheckbox)	
+		
+		$roleCheckbox.data("roleInfo",roleInfo)
+	
+	}
+	
+	
+	
+	var dbRolesParams = {
+		databaseID: collabPropsContext.databaseID
+	}
+	jsonAPIRequest("userRole/getDatabaseRoles",dbRolesParams,function(rolesInfo) {
+		
+		console.log("Got roles info: " + JSON.stringify(rolesInfo))
+		$('#adminNewUserRoleList').empty()
+		for(var roleInfoIndex = 0; roleInfoIndex<rolesInfo.length; roleInfoIndex++) {
+			var currRoleInfo = rolesInfo[roleInfoIndex]
+			addRoleToRoleCheckboxList(currRoleInfo)
+		}
+		
+	})
+	
 				
 })
