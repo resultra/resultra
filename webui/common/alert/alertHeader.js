48191f2f552215b4a0da177b79ef4375e2443566
Steve Roehling <sroehling@resultra.com>
2017-09-11 20:04:11 -0700

    Adjust the formatting of notification summaries in the alert menu.

--- webui/common/alert/alertHeader.js
+++ webui/common/alert/alertHeader.js
@@ -23,20 +23,21 @@ function initAlertHeader(databaseID) {
 			var currAlert = notificationList.alertsByID[currNotification.alertID]
 			
 			
-			var $alertListItem = $('<li><a class="alertFormLink"></a></li>')
-			
+			var $alertListItem = $('<li><a class="alertFormLink notificationListItem"></a></li>')
+						
 			var viewFormLink = '/viewItem/' + currAlert.properties.formID + '/' + currNotification.recordID
 			
 			var $alertLink = $alertListItem.find("a")
 			
 			$alertLink.attr("href",viewFormLink)
 			
-			var $alertName = $('<div class="h5"></div>')
+			var $alertName = $('<div class="h5 alertHeader"></div>')
 			$alertName.text(currAlert.name)	
 			$alertLink.append($alertName)
 			
 			var $summary = $("<div></div>")
-			$summary.text(currNotification.itemSummary)
+			var summaryText = formatNotificationSummary(fieldsByID,currNotification,currAlert)
+			$summary.text(summaryText)
 			$alertLink.append($summary)
 			
 			
@@ -56,10 +57,7 @@ function initAlertHeader(databaseID) {
 			notificationIndex++) {
 			$alertList.append(createAlertListItem(notificationIndex))
 		}
-		
-		var $divider = $('<li role="separator" class="divider"></li>')
-		$alertList.append($divider)
-	
+			
 		var $sellAllListItem = $('<li><a>See all alerts</a></li>')
 		var seeAllUrl = "/alerts/" + databaseID
 		$sellAllListItem.find("a").attr("href",seeAllUrl)
