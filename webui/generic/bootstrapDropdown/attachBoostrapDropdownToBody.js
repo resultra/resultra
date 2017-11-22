67402f634cc9b024e084fd7be06c7b5fddb4c8d9
Steve Roehling <sroehling@resultra.com>
2017-11-22 11:44:54 -0700

    Fix the alignment of dropdown menus in page headers to be right-aligned and not spill off the page.

--- webui/generic/bootstrapDropdown/attachBoostrapDropdownToBody.js
+++ webui/generic/bootstrapDropdown/attachBoostrapDropdownToBody.js
@@ -3,30 +3,47 @@
 (function () {
     // hold onto the drop down menu                                             
     var dropdownMenu;
+	var isBodyAttachDropdownMenu;
+	
 
     // and when you show it, move it to the body                                     
     $(window).on('show.bs.dropdown', function (e) {
 
         // grab the menu        
         dropdownMenu = $(e.target).find('.dropdown-menu');
+	
+		// Only attach the dropdown to the body if the option is explicitely
+		// turned on for the given dropdown menu.
+		// Using this option introduces conflicts with other dropdown menu
+		// options, notably dropdown-menu-right.
+		isBodyAttachDropdownMenu = dropdownMenu.hasClass("dropdown-menu-attach-body")
+		
+		if (isBodyAttachDropdownMenu) {
+	        // detach it and append it to the body
+	        $('body').append(dropdownMenu.detach());
 
-        // detach it and append it to the body
-        $('body').append(dropdownMenu.detach());
+	        // grab the new offset position
+	        var eOffset = $(e.target).offset();
 
-        // grab the new offset position
-        var eOffset = $(e.target).offset();
+	        // make sure to place it where it would normally go (this could be improved)
+	        dropdownMenu.css({
+	            'display': 'block',
+	                'top': eOffset.top + $(e.target).outerHeight(),
+	                'left': eOffset.left
+	        });
+		}
 
-        // make sure to place it where it would normally go (this could be improved)
-        dropdownMenu.css({
-            'display': 'block',
-                'top': eOffset.top + $(e.target).outerHeight(),
-                'left': eOffset.left
-        });
     });
+	
 
     // and when you hide it, reattach the drop down, and hide it normally                                                   
     $(window).on('hide.bs.dropdown', function (e) {
-        $(e.target).append(dropdownMenu.detach());
-        dropdownMenu.hide();
+		if (isBodyAttachDropdownMenu) {
+	        $(e.target).append(dropdownMenu.detach());
+	        dropdownMenu.hide();
+		}
+		
     });
+	
+	
 })();
