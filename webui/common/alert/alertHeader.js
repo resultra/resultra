d15fa03fa628305bd80722ff9a9ab10c9a9822f6
Steve Roehling <sroehling@resultra.com>
2018-02-13 16:06:35 -0700

    Work in progress on refreshing alert notifications.
    Use the same polling loop as the lists in the table of contents refresh the alert header.

--- webui/common/alert/alertHeader.js
+++ webui/common/alert/alertHeader.js
@@ -12,80 +12,86 @@ function initAlertHeader(databaseID,seeAllAlertsCallback) {
 		
 	}
 	
-	
-	loadNotificationListInfo(databaseID, function(notificationList,formsByID,fieldsByID) {
+	function reloadAlerts() {
+		loadNotificationListInfo(databaseID, function(notificationList,formsByID,fieldsByID) {
 		
-		function initAlertCountBadge() {
+			function initAlertCountBadge() {
 		
-			var alertCount = notificationList.numAlertsNotSeen
-			if(alertCount > 0) {
-				$alertCountBadge.show()
-				$alertCountBadge.text(alertCount)
-			} else {
-				$alertCountBadge.hide()
+				var alertCount = notificationList.numAlertsNotSeen
+				if(alertCount > 0) {
+					$alertCountBadge.show()
+					$alertCountBadge.text(alertCount)
+				} else {
+					$alertCountBadge.hide()
+				}
+			
 			}
-			
-		}
 		
-		function createAlertListItem(rawDataIndex) {
+			function createAlertListItem(rawDataIndex) {
 			
-			var currNotification = notificationList.notifications[rawDataIndex]
-			var currAlert = notificationList.alertsByID[currNotification.alertID]
+				var currNotification = notificationList.notifications[rawDataIndex]
+				var currAlert = notificationList.alertsByID[currNotification.alertID]
 				
-			var $alertListItem = $('<li><a class="alertFormLink notificationListItem"></a></li>')
+				var $alertListItem = $('<li><a class="alertFormLink notificationListItem"></a></li>')
 							
-			var $alertLink = $alertListItem.find("a")
+				var $alertLink = $alertListItem.find("a")
 			
-			$alertLink.click(function(e) {
-				clearAlertCount()
-				e.preventDefault()
-				var viewFormLink = '/viewItem/' + currAlert.properties.formID + '/' + currNotification.recordID
-				 win = window.open(viewFormLink,"_blank")
-				win.focus()
+				$alertLink.click(function(e) {
+					clearAlertCount()
+					e.preventDefault()
+					var viewFormLink = '/viewItem/' + currAlert.properties.formID + '/' + currNotification.recordID
+					 win = window.open(viewFormLink,"_blank")
+					win.focus()
 				
-			})
+				})
 			
-			var $alertName = $('<div class="h5 alertHeader"></div>')
-			$alertName.text(currAlert.name)	
-			$alertLink.append($alertName)
+				var $alertName = $('<div class="h5 alertHeader"></div>')
+				$alertName.text(currAlert.name)	
+				$alertLink.append($alertName)
 			
-			var $summary = $("<div></div>")
-			var summaryText = currNotification.caption
-			$summary.text(summaryText)
-			$alertLink.append($summary)
+				var $summary = $("<div></div>")
+				var summaryText = currNotification.caption
+				$summary.text(summaryText)
+				$alertLink.append($summary)
 			
 			
-			var alertMoment = moment(currNotification.timestamp)
-			var alertTime = alertMoment.calendar() + " (" + alertMoment.fromNow() + ")"
-			var $alertTime = "<div><small>" + alertTime + "</small></div>"
-			$alertLink.append($alertTime)
+				var alertMoment = moment(currNotification.timestamp)
+				var alertTime = alertMoment.calendar() + " (" + alertMoment.fromNow() + ")"
+				var $alertTime = "<div><small>" + alertTime + "</small></div>"
+				$alertLink.append($alertTime)
 					
-			return $alertListItem
-		}
-		
-		var $alertList = $header.find("ul")
-		$alertList.empty()
-		var maxNotificationDisplay = 5
-		for(var notificationIndex = 0; 
-			(notificationIndex < notificationList.notifications.length) && (notificationIndex < maxNotificationDisplay);
-			notificationIndex++) {
-			$alertList.append(createAlertListItem(notificationIndex))
-		}
-			
-		var $sellAllListItem = $('<li><a>See all alerts</a></li>')
-		var seeAllUrl = "/alerts/" + databaseID
-		var $seeAllLink = $sellAllListItem.find("a")
-		$alertList.append($sellAllListItem)
-		
-		$seeAllLink.click(function(e) {
-			e.preventDefault()
-			clearAlertCount()
-			if(seeAllAlertsCallback !== undefined) {
-				seeAllAlertsCallback()
+				return $alertListItem
 			}
-		})
 		
-		initAlertCountBadge()
+			var $alertList = $header.find("ul")
+			$alertList.empty()
+			var maxNotificationDisplay = 5
+			for(var notificationIndex = 0; 
+				(notificationIndex < notificationList.notifications.length) && (notificationIndex < maxNotificationDisplay);
+				notificationIndex++) {
+				$alertList.append(createAlertListItem(notificationIndex))
+			}
+			
+			var $sellAllListItem = $('<li><a>See all alerts</a></li>')
+			var seeAllUrl = "/alerts/" + databaseID
+			var $seeAllLink = $sellAllListItem.find("a")
+			$alertList.append($sellAllListItem)
 		
-	})
+			$seeAllLink.click(function(e) {
+				e.preventDefault()
+				clearAlertCount()
+				if(seeAllAlertsCallback !== undefined) {
+					seeAllAlertsCallback()
+				}
+			})
+		
+			initAlertCountBadge()
+		
+		})		
+	}
+	
+	initRefreshPollingLoop(10,reloadAlerts)
+	
+	
+
 }
