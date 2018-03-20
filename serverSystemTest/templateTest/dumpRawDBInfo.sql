8dd8826f684582aefd921a631c1d8396e66eee72
Steve Roehling <sroehling@resultra.com>
2018-03-20 09:49:04 -0700

    Add a raw version of the SQL dump to the template export test suite.

---
+++ serverSystemTest/templateTest/dumpRawDBInfo.sql
@@ -0,0 +1,18 @@
+select "TRACKER->",* from databases order by name;
+select "-----------------------------------------";
+select "FORM->",* from forms order by name;
+select "-----------------------------------------";
+select "FIELD->",* from fields order by name;
+select "-----------------------------------------";
+select "TABLE VIEW->",* from table_views order by name;
+select "-----------------------------------------";
+select "TABLE COL->",* from table_view_columns order by column_id;
+select "-----------------------------------------";
+select "DASHBOARD->",* from dashboards order by name;
+select "-----------------------------------------";
+select "LIST->",* from item_lists order by name;
+select "-----------------------------------------";
+select "NEW ITEM LINK---->",* from form_links order by name;
+select "-----------------------------------------";
+select "VALUE LIST-->",* from value_lists order by name;
+ 
