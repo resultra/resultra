6edf4429803f2bed9752a2d3ab312e5ac177b7e8
Steve Roehling <sroehling@resultra.com>
2018-04-19 09:00:12 -0700

    Prevent the creation of duplicate roles and alerts from their dialog boxes.

--- webui/admin/alerts/alertList/newAlertDialog.js
+++ webui/admin/alerts/alertList/newAlertDialog.js
@@ -45,26 +45,37 @@ function openNewAlertDialog(databaseID) {
 	var $newAlertDialog = $('#newAlertDialog')
 		
 	$newAlertDialog.modal('show')
-	
+
+	var alertCreated = false
 	initButtonClickHandler('#newAlertSaveButton',function() {
 		console.log("New alert save button clicked")
 		if($newAlertDialogForm.valid()) {	
-			var newAlertParams = { 
-				parentDatabaseID: databaseID, 
-				name: $alertNameInput.val(),
-				formID: $formSelection.val()}
-			jsonAPIRequest("alert/new",newAlertParams,function(newAlertInfo) {
-				console.log("Created new alert: " + JSON.stringify(newAlertInfo))
-				$newAlertDialog.modal('hide')
+			
+			if (alertCreated === false) {
 				
-				var editPropsContentURL = '/admin/alert/' + newAlertInfo.alertID
-				setSettingsPageContent(editPropsContentURL,function() {
-					initAlertSettingsAdminPageContent(databaseID,newAlertInfo)
+				// Only support the creation of a single alert from the dialog box.
+				// This prevents the creation of multiple alerts with the same properties,
+				// in the event the user "double tapped" the OK button.
+				alertCreated = true
+				
+				var newAlertParams = { 
+					parentDatabaseID: databaseID, 
+					name: $alertNameInput.val(),
+					formID: $formSelection.val()}
+				jsonAPIRequest("alert/new",newAlertParams,function(newAlertInfo) {
+					console.log("Created new alert: " + JSON.stringify(newAlertInfo))
+					$newAlertDialog.modal('hide')
+				
+					var editPropsContentURL = '/admin/alert/' + newAlertInfo.alertID
+					setSettingsPageContent(editPropsContentURL,function() {
+						initAlertSettingsAdminPageContent(databaseID,newAlertInfo)
+					})
+				
 				})
 				
-			})
+				
+			}
 			
-
 		}
 	})
 	
--- webui/admin/userRole/userRoleList/newUserRoleDialog.js
+++ webui/admin/userRole/userRoleList/newUserRoleDialog.js
@@ -23,25 +23,38 @@ function openNewUserRoleDialog(pageContext) {
 	
 	$newRoleDialog.modal('show')
 	
+	var roleCreated = false
+	
 	initButtonClickHandler('#newRoleSaveButton',function() {
 		console.log("Save button clicked")
 		if($newRoleRoleNamePanelForm.valid()) {
-			var newRoleParams = {
-				databaseID: pageContext.databaseID,
-				roleName: $roleNameInput.val()
-			}
-			console.log("Saving new user role: params=" + JSON.stringify(newRoleParams))
-		
-			jsonAPIRequest("userRole/newRole",newRoleParams,function(newRoleInfo) {
-		
-				$newRoleDialog.modal('hide')	
+			
+			if (roleCreated === false) {
+				// Only allow creation of a single role from the dialog box.
+				// Prevent the case where the user "double taps" the OK button,
+				// accidently creating 2 roles with the same properties.
+				roleCreated = true
 				
-				var userRoleContentURL = '/admin/userRole/' + newRoleInfo.roleID
-				setSettingsPageContent(userRoleContentURL, function() {
-					initUserRolePropsAdminSettingsPageContent(pageContext,newRoleInfo)
-				})
+				
+				var newRoleParams = {
+					databaseID: pageContext.databaseID,
+					roleName: $roleNameInput.val()
+				}
+				console.log("Saving new user role: params=" + JSON.stringify(newRoleParams))
 		
-			})
+				jsonAPIRequest("userRole/newRole",newRoleParams,function(newRoleInfo) {
+		
+					$newRoleDialog.modal('hide')	
+				
+					var userRoleContentURL = '/admin/userRole/' + newRoleInfo.roleID
+					setSettingsPageContent(userRoleContentURL, function() {
+						initUserRolePropsAdminSettingsPageContent(pageContext,newRoleInfo)
+					})
+		
+				})
+				
+			}
+			
 		}
 	})
 		
